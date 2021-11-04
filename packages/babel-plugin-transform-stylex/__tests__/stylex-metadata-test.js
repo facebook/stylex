e76b30a751b593b378ae7a77b34ef6ac4e4721f1
Nicolas Gallagher <necolas@fb.com>
2021-11-04 12:12:58 -0700

    Refactor babel plugin metadata
    Rely on inline snapshots
    
    Ref #63

--- packages/babel-plugin-transform-stylex/__tests__/stylex-metadata-test.js
+++ packages/babel-plugin-transform-stylex/__tests__/stylex-metadata-test.js
@@ -9,227 +9,94 @@
 
 jest.autoMockOff();
 
-const { transformSync: babelTransform } = require('@babel/core');
+const { transformSync } = require('@babel/core');
+const stylexPlugin = require('../src/index');
 
 function transform(source, opts = {}) {
-  return babelTransform(source, {
+  return transformSync(source, {
     filename: opts.filename,
-    plugins: [[require('../src/index'), opts]],
+    parserOpts: {
+      flow: {
+        all: true,
+      },
+    },
+    plugins: [stylexPlugin, opts],
   });
 }
 
-test('ensure metadata is correctly set', () => {
-  const res = transform(` 
-    const styles = stylex.create({
-      foo: {
-        color: 'red',
-        height: 5,
+describe('babel-plugin-transform-stylex', () => {
+  describe('[metadata] plugin metadata', () => {
+    test('stylex metadata is correctly set', () => {
+      const output = transform(`
+        const styles = stylex.create({
+          foo: {
+            color: 'red',
+            height: 5,
+            ':hover': {
+              start: 10,
+            },
+            '@media (min-width: 1000px)': {
+              end: 5
+            }
+          },
+        });
 
-        ':hover': {
-          width: 10,
+        const name = stylex.keyframes({
+          from: {
+            start: 0,
+          },
+          to: {
+            start: 100,
+          }
+        });
+      `);
+
+      expect(output.metadata).toMatchInlineSnapshot(`
+        Object {
+          "stylex": Array [
+            Array [
+              "h3ivgpu3",
+              Object {
+                "ltr": ".h3ivgpu3{color:red}",
+                "rtl": null,
+              },
+              1,
+            ],
+            Array [
+              "sws6tvwg",
+              Object {
+                "ltr": ".sws6tvwg{height:5px}",
+                "rtl": null,
+              },
+              1,
+            ],
+            Array [
+              "beji1480",
+              Object {
+                "ltr": ".beji1480:hover{left:10px}",
+                "rtl": ".beji1480:hover{right:10px}",
+              },
+              8,
+            ],
+            Array [
+              "afainbk7",
+              Object {
+                "ltr": "@media (min-width: 1000px){.afainbk7.afainbk7{right:5px}}",
+                "rtl": "@media (min-width: 1000px){.afainbk7.afainbk7{left:5px}}",
+              },
+              2,
+            ],
+            Array [
+              "gd0gnj7z-B",
+              Object {
+                "ltr": "@keyframes gd0gnj7z-B{from{left:0;}to{left:100px;}}",
+                "rtl": "@keyframes gd0gnj7z-B{from{right:0;}to{right:100px;}}",
+              },
+              1,
+            ],
+          ],
         }
-      },
+      `);
     });
-
-    const name = stylex.keyframes({
-      from: {
-        left: 0,
-      },
-
-      to: {
-        left: 100,
-      }
-    });
-  `);
-
-  expect(res.metadata).toEqual({
-    stylex: [
-      ['h3ivgpu3', { ltr: '.h3ivgpu3{color:red}', rtl: null }, 1],
-      ['sws6tvwg', { ltr: '.sws6tvwg{height:5px}', rtl: null }, 1],
-      ['ennk8xi2', { ltr: '.ennk8xi2:hover{width:10px}', rtl: null }, 8],
-      [
-        'gd0gnj7z-B',
-        {
-          ltr: '@keyframes gd0gnj7z-B{from{left:0;}to{left:100px;}}',
-          rtl: null,
-        },
-        1,
-      ],
-    ],
-  });
-});
-
-test('ensure stylex.create metadata is correctly set', () => {
-  const res = transform(`
-    const styles = stylex.create({
-      foo: {
-        color: 'red',
-        height: 5,
-
-        ':hover': {
-          width: 10,
-        }
-      },
-    });
-
-    const name = stylex.keyframes({
-      from: {
-        left: 0,
-      },
-
-      to: {
-        left: 100,
-      }
-    });
-  `);
-
-  expect(res.metadata).toEqual({
-    stylex: [
-      ['h3ivgpu3', { ltr: '.h3ivgpu3{color:red}', rtl: null }, 1],
-      ['sws6tvwg', { ltr: '.sws6tvwg{height:5px}', rtl: null }, 1],
-      ['ennk8xi2', { ltr: '.ennk8xi2:hover{width:10px}', rtl: null }, 8],
-      [
-        'gd0gnj7z-B',
-        {
-          ltr: '@keyframes gd0gnj7z-B{from{left:0;}to{left:100px;}}',
-          rtl: null,
-        },
-        1,
-      ],
-    ],
-  });
-});
-
-test('ensure spreads work correctly', () => {
-  const res = transform(`
-    const styles = stylex.create({
-      foo: {
-        ...stylex.flexBase,
-        color: 'red',
-        height: 5,
-
-        ':hover': {
-          width: 10,
-        }
-      },
-    });
-  `);
-
-  expect(res.metadata).toEqual({
-    stylex: [
-      // Spread Styles
-      ['o9w3sbdw', { ltr: '.o9w3sbdw{align-items:stretch}', rtl: null }, 1],
-      ['gewhe1h2', { ltr: '.gewhe1h2{border-style:solid}', rtl: null }, 0.2],
-      ['gcovof34', { ltr: '.gcovof34{border-width:0}', rtl: null }, 0.2],
-      ['bdao358l', { ltr: '.bdao358l{box-sizing:border-box}', rtl: null }, 1],
-      ['alzwoclg', { ltr: '.alzwoclg{display:flex}', rtl: null }, 1],
-      ['cqf1kptm', { ltr: '.cqf1kptm{flex-direction:column}', rtl: null }, 1],
-      ['cgu29s5g', { ltr: '.cgu29s5g{flex-grow:1}', rtl: null }, 1],
-      ['i15ihif8', { ltr: '.i15ihif8{flex-shrink:1}', rtl: null }, 1],
-      [
-        'sl27f92c',
-        { ltr: '.sl27f92c{justify-content:space-between}', rtl: null },
-        1,
-      ],
-      ['kjdc1dyq', { ltr: '.kjdc1dyq{margin-bottom:0}', rtl: null }, 1],
-      [
-        'l7ghb35v',
-        { ltr: '.l7ghb35v{margin-right:0}', rtl: '.l7ghb35v{margin-left:0}' },
-        1,
-      ],
-      [
-        'kmwttqpk',
-        { ltr: '.kmwttqpk{margin-left:0}', rtl: '.kmwttqpk{margin-right:0}' },
-        1,
-      ],
-      ['m8h3af8h', { ltr: '.m8h3af8h{margin-top:0}', rtl: null }, 1],
-      ['dnr7xe2t', { ltr: '.dnr7xe2t{min-height:0}', rtl: null }, 1],
-      ['aeinzg81', { ltr: '.aeinzg81{min-width:0}', rtl: null }, 1],
-      ['rl78xhln', { ltr: '.rl78xhln{padding-bottom:0}', rtl: null }, 1],
-      [
-        'oxkhqvkx',
-        { ltr: '.oxkhqvkx{padding-right:0}', rtl: '.oxkhqvkx{padding-left:0}' },
-        1,
-      ],
-      [
-        'nch0832m',
-        { ltr: '.nch0832m{padding-left:0}', rtl: '.nch0832m{padding-right:0}' },
-        1,
-      ],
-      ['srn514ro', { ltr: '.srn514ro{padding-top:0}', rtl: null }, 1],
-      ['om3e55n1', { ltr: '.om3e55n1{position:relative}', rtl: null }, 1],
-      ['g4tp4svg', { ltr: '.g4tp4svg{z-index:0}', rtl: null }, 1],
-      // Other Styles
-      ['h3ivgpu3', { ltr: '.h3ivgpu3{color:red}', rtl: null }, 1],
-      ['sws6tvwg', { ltr: '.sws6tvwg{height:5px}', rtl: null }, 1],
-      ['ennk8xi2', { ltr: '.ennk8xi2:hover{width:10px}', rtl: null }, 8],
-    ],
-  });
-});
-
-test('ensure spreads work correctly with stylex.create', () => {
-  const res = transform(`
-    const styles = stylex.create({
-      foo: {
-        ...stylex.flexBase,
-        color: 'red',
-        height: 5,
-
-        ':hover': {
-          width: 10,
-        }
-      },
-    });
-  `);
-
-  expect(res.metadata).toEqual({
-    stylex: [
-      // Spread Styles
-      ['o9w3sbdw', { ltr: '.o9w3sbdw{align-items:stretch}', rtl: null }, 1],
-      ['gewhe1h2', { ltr: '.gewhe1h2{border-style:solid}', rtl: null }, 0.2],
-      ['gcovof34', { ltr: '.gcovof34{border-width:0}', rtl: null }, 0.2],
-      ['bdao358l', { ltr: '.bdao358l{box-sizing:border-box}', rtl: null }, 1],
-      ['alzwoclg', { ltr: '.alzwoclg{display:flex}', rtl: null }, 1],
-      ['cqf1kptm', { ltr: '.cqf1kptm{flex-direction:column}', rtl: null }, 1],
-      ['cgu29s5g', { ltr: '.cgu29s5g{flex-grow:1}', rtl: null }, 1],
-      ['i15ihif8', { ltr: '.i15ihif8{flex-shrink:1}', rtl: null }, 1],
-      [
-        'sl27f92c',
-        { ltr: '.sl27f92c{justify-content:space-between}', rtl: null },
-        1,
-      ],
-      ['kjdc1dyq', { ltr: '.kjdc1dyq{margin-bottom:0}', rtl: null }, 1],
-      [
-        'l7ghb35v',
-        { ltr: '.l7ghb35v{margin-right:0}', rtl: '.l7ghb35v{margin-left:0}' },
-        1,
-      ],
-      [
-        'kmwttqpk',
-        { ltr: '.kmwttqpk{margin-left:0}', rtl: '.kmwttqpk{margin-right:0}' },
-        1,
-      ],
-      ['m8h3af8h', { ltr: '.m8h3af8h{margin-top:0}', rtl: null }, 1],
-      ['dnr7xe2t', { ltr: '.dnr7xe2t{min-height:0}', rtl: null }, 1],
-      ['aeinzg81', { ltr: '.aeinzg81{min-width:0}', rtl: null }, 1],
-      ['rl78xhln', { ltr: '.rl78xhln{padding-bottom:0}', rtl: null }, 1],
-      [
-        'oxkhqvkx',
-        { ltr: '.oxkhqvkx{padding-right:0}', rtl: '.oxkhqvkx{padding-left:0}' },
-        1,
-      ],
-      [
-        'nch0832m',
-        { ltr: '.nch0832m{padding-left:0}', rtl: '.nch0832m{padding-right:0}' },
-        1,
-      ],
-      ['srn514ro', { ltr: '.srn514ro{padding-top:0}', rtl: null }, 1],
-      ['om3e55n1', { ltr: '.om3e55n1{position:relative}', rtl: null }, 1],
-      ['g4tp4svg', { ltr: '.g4tp4svg{z-index:0}', rtl: null }, 1],
-      // Other Styles
-      ['h3ivgpu3', { ltr: '.h3ivgpu3{color:red}', rtl: null }, 1],
-      ['sws6tvwg', { ltr: '.sws6tvwg{height:5px}', rtl: null }, 1],
-      ['ennk8xi2', { ltr: '.ennk8xi2:hover{width:10px}', rtl: null }, 8],
-    ],
   });
 });
