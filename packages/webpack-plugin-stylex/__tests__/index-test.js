355bd94fd37a399b1d068affeb4037af0c5582b1
Will Hastings <willhastings@fb.com>
2021-11-29 17:47:29 -0800

    Have webpack-plugin-stylex remove stylex.inject calls
    Ref: #53

--- packages/webpack-plugin-stylex/__tests__/index-test.js
+++ packages/webpack-plugin-stylex/__tests__/index-test.js
@@ -9,7 +9,6 @@
 
 const memfs = require('memfs');
 const path = require('path');
-const stylexBabelPlugin = require('babel-plugin-transform-stylex');
 const StylexPlugin = require('../src/index');
 const webpack = require('webpack');
 
@@ -17,6 +16,7 @@ const webpack = require('webpack');
  * Webpack compiler factory
  */
 function createCompiler(fixture, pluginOptions = {}, config = {}) {
+  const stylexPlugin = new StylexPlugin(pluginOptions);
   const fullConfig = {
     context: path.resolve(__dirname, './__fixtures__'),
     entry: path.resolve(__dirname, './__fixtures__', fixture),
@@ -31,18 +31,14 @@ function createCompiler(fixture, pluginOptions = {}, config = {}) {
           use: {
             loader: require.resolve('babel-loader'),
             options: {
-              plugins: [[stylexBabelPlugin, { dev: true, ...pluginOptions }]],
-              metadataSubscribers: [StylexPlugin.stylexMetadataSubscription],
+              plugins: [stylexPlugin.babelPlugin],
+              metadataSubscribers: [stylexPlugin.stylexMetadataSubscription],
             },
           },
         },
       ],
     },
-    plugins: [
-      new StylexPlugin({
-        filename: 'stylex.css',
-      }),
-    ],
+    plugins: [stylexPlugin],
     devtool: false, //'cheap-source-map',
     externals: {
       // Remove stylex runtime from bundle
@@ -55,6 +51,9 @@ function createCompiler(fixture, pluginOptions = {}, config = {}) {
     optimization: {
       // Keep output readable
       minimize: false,
+      // Keep module IDs from changing between test runs
+      // and failing the snapshot comparison
+      moduleIds: 'named',
       // Remove webpack runtime from bundle
       runtimeChunk: 'single',
     },
@@ -72,22 +71,34 @@ function createCompiler(fixture, pluginOptions = {}, config = {}) {
   return compiler;
 }
 
+function getTargetFile(asset) {
+  let targetFile = asset;
+  const queryStringIdx = targetFile.indexOf('?');
+  if (queryStringIdx >= 0) {
+    targetFile = targetFile.substr(0, queryStringIdx);
+  }
+  return targetFile;
+}
+
 /**
  * Read assets in the webpack output
  */
 function readAsset(asset, compiler, stats) {
   const usedFs = compiler.outputFileSystem;
   const outputPath = stats.compilation.outputOptions.path;
-  let targetFile = asset;
-  const queryStringIdx = targetFile.indexOf('?');
-  if (queryStringIdx >= 0) {
-    targetFile = targetFile.substr(0, queryStringIdx);
-  }
+  const targetFile = getTargetFile(asset);
   try {
     return usedFs.readFileSync(path.join(outputPath, targetFile)).toString();
   } catch (error) {
     return error.toString();
   }
+}
+
+function assetExists(asset, compiler, stats) {
+  const usedFs = compiler.outputFileSystem;
+  const outputPath = stats.compilation.outputOptions.path;
+  const targetFile = getTargetFile(asset);
+  return usedFs.existsSync(path.join(outputPath, targetFile));
 }
 
 describe('webpack-plugin-stylex', () => {
@@ -100,14 +111,14 @@ describe('webpack-plugin-stylex', () => {
       const js = readAsset('main.js', compiler, stats);
 
       expect(css).toMatchInlineSnapshot(`
-        ".alzwoclg{display:flex}
-        html:not([dir='rtl']) .a60616oh{margin-left:10px}
-        html[dir='rtl'] .a60616oh{margin-right:10px}
-        .h9hgjgce{margin-block-start:99px}
-        .mjo95iq7{height:500px}
-        .b6ax4al1{display:block}
-        .mfclru0v{width:100%}
-        .ew9r2zzs:hover{background:red}"
+        ".p357zi0d{display:flex}
+        html:not([dir='rtl']) .a3oefunm{margin-left:10px}
+        html[dir='rtl'] .a3oefunm{margin-right:10px}
+        .bjgvxnpl{margin-block-start:99px}
+        .cctpw5f5{height:500px}
+        .f804f6gw{display:block}
+        .ln8gz9je{width:100%}
+        .lq9oatf1:hover{background:red}"
       `);
 
       expect(js).toMatchInlineSnapshot(`
@@ -118,7 +129,7 @@ describe('webpack-plugin-stylex', () => {
         exports.ids = [179];
         exports.modules = {
 
-        /***/ 899:
+        /***/ \\"./index.js\\":
         /***/ ((__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) => {
 
 
@@ -132,13 +143,10 @@ describe('webpack-plugin-stylex', () => {
 
 
 
-        external_stylex_default().inject(\\".b6ax4al1{display:block}\\", 1);
-        external_stylex_default().inject(\\".mfclru0v{width:100%}\\", 1);
         const styles = {
           bar: {
-            otherStyles__bar: \\"otherStyles__bar\\",
-            display: \\"b6ax4al1\\",
-            width: \\"mfclru0v\\"
+            display: \\"f804f6gw\\",
+            width: \\"ln8gz9je\\"
           }
         };
         /* harmony default export */ const otherStyles_0 = ((/* unused pure expression or super */ null && (styles)));
@@ -167,21 +175,14 @@ describe('webpack-plugin-stylex', () => {
 
 
 
-        external_stylex_default().inject(\\".alzwoclg{display:flex}\\", 1);
-        external_stylex_default().inject(\\".a60616oh{margin-left:10px}\\", 1, \\".a60616oh{margin-right:10px}\\");
-        external_stylex_default().inject(\\".h9hgjgce{margin-block-start:99px}\\", 1);
-        external_stylex_default().inject(\\".mjo95iq7{height:500px}\\", 1);
-        external_stylex_default().inject(\\".ew9r2zzs:hover{background:red}\\", 7.1);
         const index_styles = {
           foo: {
-            index__foo: \\"index__foo\\",
-            display: \\"alzwoclg\\",
-            marginStart: \\"a60616oh\\",
-            marginBlockStart: \\"h9hgjgce\\",
-            height: \\"mjo95iq7\\",
+            display: \\"p357zi0d\\",
+            marginStart: \\"a3oefunm\\",
+            marginBlockStart: \\"bjgvxnpl\\",
+            height: \\"cctpw5f5\\",
             ':hover': {
-              index__foo: \\"index__foo\\",
-              background: \\"ew9r2zzs\\"
+              background: \\"lq9oatf1\\"
             }
           }
         };
@@ -198,11 +199,127 @@ describe('webpack-plugin-stylex', () => {
         var __webpack_require__ = require(\\"./runtime.js\\");
         __webpack_require__.C(exports);
         var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-        var __webpack_exports__ = (__webpack_exec__(899));
+        var __webpack_exports__ = (__webpack_exec__(\\"./index.js\\"));
 
         })();"
       `);
       done();
+    });
+  });
+
+  describe('when in dev mode', () => {
+    it('preserves stylex.inject calls and does not extract CSS', (done) => {
+      const compiler = createCompiler('index.js', { dev: true });
+      compiler.run((error, stats) => {
+        expect(error).toBe(null);
+
+        const cssExists = assetExists('stylex.css', compiler, stats);
+        const js = readAsset('main.js', compiler, stats);
+
+        expect(cssExists).toEqual(false);
+
+        expect(js).toMatchInlineSnapshot(`
+          "\\"use strict\\";
+          (() => {
+          var exports = {};
+          exports.id = 179;
+          exports.ids = [179];
+          exports.modules = {
+
+          /***/ \\"./index.js\\":
+          /***/ ((__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) => {
+
+
+          // UNUSED EXPORTS: default
+
+          ;// CONCATENATED MODULE: external \\"stylex\\"
+          const external_stylex_namespaceObject = stylex;
+          var external_stylex_default = /*#__PURE__*/__webpack_require__.n(external_stylex_namespaceObject);
+          ;// CONCATENATED MODULE: ./otherStyles.js
+          // otherStyles.js
+
+
+
+
+          if (__DEV__) {
+            external_stylex_default().inject(\\".f804f6gw{display:block}\\", 1);
+            external_stylex_default().inject(\\".ln8gz9je{width:100%}\\", 1);
+          }
+
+          const styles = {
+            bar: {
+              otherStyles__bar: \\"otherStyles__bar\\",
+              display: \\"f804f6gw\\",
+              width: \\"ln8gz9je\\"
+            }
+          };
+          /* harmony default export */ const otherStyles_0 = ((/* unused pure expression or super */ null && (styles)));
+          ;// CONCATENATED MODULE: ./npmStyles.js
+          // npmStyles.js
+
+
+
+
+
+          external_stylex_default().inject('.rse6dlih{display:inline}', 1);
+          external_stylex_default().inject('.ezi3dscr{width:50%}', 1);
+          const npmStyles_styles = {
+            baz: {
+              display: 'rse6dlih',
+              width: 'ezi3dscr',
+            },
+          };
+
+          /* harmony default export */ const npmStyles_0 = ((/* unused pure expression or super */ null && (npmStyles_styles)));
+
+          ;// CONCATENATED MODULE: ./index.js
+          // index.js
+
+
+
+
+
+
+          if (__DEV__) {
+            external_stylex_default().inject(\\".p357zi0d{display:flex}\\", 1);
+            external_stylex_default().inject(\\".a3oefunm{margin-left:10px}\\", 1, \\".a3oefunm{margin-right:10px}\\");
+            external_stylex_default().inject(\\".bjgvxnpl{margin-block-start:99px}\\", 1);
+            external_stylex_default().inject(\\".cctpw5f5{height:500px}\\", 1);
+            external_stylex_default().inject(\\".lq9oatf1:hover{background:red}\\", 7.1);
+          }
+
+          const index_styles = {
+            foo: {
+              index__foo: \\"index__foo\\",
+              display: \\"p357zi0d\\",
+              marginStart: \\"a3oefunm\\",
+              marginBlockStart: \\"bjgvxnpl\\",
+              height: \\"cctpw5f5\\",
+              ':hover': {
+                index__foo: \\"index__foo\\",
+                background: \\"lq9oatf1\\"
+              }
+            }
+          };
+          function App() {
+            return stylex(otherStyles.bar, index_styles.foo, npmStyles.baz);
+          }
+
+          /***/ })
+
+          };
+          ;
+
+          // load runtime
+          var __webpack_require__ = require(\\"./runtime.js\\");
+          __webpack_require__.C(exports);
+          var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+          var __webpack_exports__ = (__webpack_exec__(\\"./index.js\\"));
+
+          })();"
+        `);
+        done();
+      });
     });
   });
 });
--- packages/webpack-plugin-stylex/src/index.js
+++ packages/webpack-plugin-stylex/src/index.js
@@ -10,19 +10,26 @@ const webpack = require('webpack');
 
 const PLUGIN_NAME = 'stylex';
 
+const IS_DEV_ENV =
+  process.env.NODE_ENV === 'development' ||
+  process.env.BABEL_ENV === 'development';
+
 const { RawSource } = webpack.sources;
 
 class StylexPlugin {
-  static stylexMetadataSubscription = 'stylexMetadata';
+  stylexMetadataSubscription = 'stylexMetadata';
 
-  constructor({ filename = 'stylex.css' } = {}) {
+  constructor({ dev = IS_DEV_ENV, filename = 'stylex.css' } = {}) {
+    this.dev = dev;
     this.filename = filename;
+    this.babelPlugin = [stylexBabelPlugin, { dev, stylexSheetName: filename }];
   }
 
   apply(compiler) {
     const logger = compiler.getInfrastructureLogger(PLUGIN_NAME);
 
     let stylexRules = [];
+    const { dev, stylexMetadataSubscription } = this;
 
     // Read stylex metadata from individual modules and collect it
     compiler.hooks.make.tap(PLUGIN_NAME, (compilation) => {
@@ -30,10 +37,8 @@ class StylexPlugin {
         PLUGIN_NAME,
         function (context, module) {
           const resourceName = module.resource;
-          context[StylexPlugin.stylexMetadataSubscription] = function (
-            metadata
-          ) {
-            if (metadata.stylex != null && metadata.stylex.length > 0) {
+          context[stylexMetadataSubscription] = function (metadata) {
+            if (!dev && metadata.stylex != null && metadata.stylex.length > 0) {
               stylexRules = stylexRules.concat(metadata.stylex);
               logger.debug(
                 `Read stylex from ${resourceName}:`,
@@ -47,9 +52,11 @@ class StylexPlugin {
       // Consumer collected rules and emit the stylex CSS asset
       compilation.hooks.additionalAssets.tap(PLUGIN_NAME, () => {
         try {
-          const collectedCSS =
-            stylexBabelPlugin.processStylexRules(stylexRules);
-          compilation.emitAsset(this.filename, new RawSource(collectedCSS));
+          if (stylexRules.length > 0) {
+            const collectedCSS =
+              stylexBabelPlugin.processStylexRules(stylexRules);
+            compilation.emitAsset(this.filename, new RawSource(collectedCSS));
+          }
         } catch (e) {
           compilation.errors.push(e);
         }
