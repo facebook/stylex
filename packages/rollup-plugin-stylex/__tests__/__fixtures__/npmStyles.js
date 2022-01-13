6a878c90b031e41030dde17cac7e7cbc7582183d
Will Hastings <willhastings@fb.com>
2022-01-12 16:12:36 -0800

    Process styles from npm module in rollup plugin

--- packages/rollup-plugin-stylex/__tests__/__fixtures__/npmStyles.js
+++ packages/rollup-plugin-stylex/__tests__/__fixtures__/npmStyles.js
@@ -4,13 +4,12 @@
 
 import stylex from 'stylex';
 
-stylex.inject('.rse6dlih{display:inline}', 1);
-stylex.inject('.ezi3dscr{width:50%}', 1);
-const styles = {
+const styles = stylex.create({
   baz: {
-    display: 'rse6dlih',
-    width: 'ezi3dscr',
+    display: 'inline',
+    height: 500,
+    width: '50%',
   },
-};
+});
 
 export default styles;
