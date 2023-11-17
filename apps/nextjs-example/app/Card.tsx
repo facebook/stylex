7837c11c472adce3dde6422b3691badfe8aa1cf1
Naman Goel <nmn@users.noreply.github.com>
2023-11-17 01:11:16 -0800

    Add code of conduct and copyright headers

--- apps/nextjs-example/app/Card.tsx
+++ apps/nextjs-example/app/Card.tsx
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import stylex from '@stylexjs/stylex';
 import { globalTokens as $, spacing, text } from './globalTokens.stylex';
 import '@stylexjs/open-props/lib/colors.stylex';
