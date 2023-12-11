27dcb735d604dfe5cfb27f13d638b5896a63064a
nedjulius <julius.nedzinskas@gmail.com>
2023-12-11 13:52:59 -0500

    Remove comment

--- apps/esbuild-example/.eslintrc.js
+++ apps/esbuild-example/.eslintrc.js
@@ -10,8 +10,6 @@
 module.exports = {
   plugins: ['@stylexjs'],
   rules: {
-    // The Eslint rule still needs work, but you can
-    // enable it to test things out.
     '@stylexjs/valid-styles': 'error',
     'ft-flow/space-after-type-colon': 0,
     'ft-flow/no-types-missing-file-annotation': 0,
