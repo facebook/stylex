44076e811c61e57691c2855d2d609df027145c01
Julius <45975811+nedjulius@users.noreply.github.com>
2024-01-07 15:44:24 +0200

    feat: add `esbuild` plugin (#113)
    * Add eslint-plugin package
    
    * Add initial test suite
    
    * Improve CSS handling and tests
    
    * Address some of the comments
    
    * Add babel scripts for jsx, flow and ts
    
    * Add package diff
    
    * Add snapshots and fix nits
    
    * Fix rename bug
    
    * Add esbuild example app
    
    * Fix incorrect loaders
    
    * Update package-lock.json
    
    * Remove unnecessary file
    
    * Fix some nits in example
    
    * Add additional style to text fixture
    
    * Remove ext from import in example
    
    * Address some of the comments
    
    * Update package-lock.json
    
    * Add missing function call and snapshot
    
    * Remove comment
    
    * Pluralize function name
    
    * Add types
    
    * Fix build script
    
    * Add docs
    
    * Fix tabulation
    
    * Retake snapshots
    
    * Format prettier
    
    * Add more missing types
    
    * Run formatter
    
    * Fix formatting
    
    * Lock dependency and fix import
    
    * Disable unused key
    
    * Add docs
    
    * Update package-lock.json
    
    * Update package-lock.json
    
    * Add snapshots
    
    * Address comments

---
+++ packages/esbuild-plugin/src/index.js
@@ -0,0 +1,179 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict
+ */
+
+import path from 'path';
+// import { readFile, writeFile, mkdir } from 'fs/promises';
+import { promises } from 'fs';
+import { transformAsync, type PluginItem } from '@babel/core';
+import stylexBabelPlugin from '@stylexjs/babel-plugin';
+import flowSyntaxPlugin from '@babel/plugin-syntax-flow';
+import hermesParserPlugin from 'babel-plugin-syntax-hermes-parser';
+import typescriptSyntaxPlugin from '@babel/plugin-syntax-typescript';
+import jsxSyntaxPlugin from '@babel/plugin-syntax-jsx';
+import { utils } from '@stylexjs/shared';
+import type { Options, Rule } from '@stylexjs/babel-plugin';
+import type { Plugin, PluginBuild, BuildResult, OnLoadArgs } from 'esbuild';
+
+const { readFile, writeFile, mkdir } = promises;
+
+const PACKAGE_NAME = 'esbuild-plugin-stylex';
+
+const IS_DEV_ENV =
+  process.env.NODE_ENV === 'development' ||
+  process.env.BABEL_ENV === 'development';
+
+const STYLEX_PLUGIN_ONLOAD_FILTER = /\.(jsx|js|tsx|ts|mjs|cjs|mts|cts)$/;
+
+export type PluginOptions = $ReadOnly<{
+  ...Partial<Options>,
+  generatedCSSFileName?: string,
+  stylexImports?: $ReadOnlyArray<string>,
+  babelConfig?: $ReadOnly<{
+    plugins?: $ReadOnlyArray<PluginItem>,
+    presets?: $ReadOnlyArray<PluginItem>,
+  }>,
+  useCSSLayers?: boolean,
+  ...
+}>;
+
+export default function stylexPlugin({
+  dev = IS_DEV_ENV,
+  unstable_moduleResolution = { type: 'commonJS', rootDir: process.cwd() },
+  stylexImports = ['@stylexjs/stylex'],
+  generatedCSSFileName = path.resolve(__dirname, 'stylex.css'),
+  babelConfig: { plugins = [], presets = [] } = {},
+  useCSSLayers = false,
+  ...options
+}: PluginOptions = {}): Plugin {
+  return {
+    name: PACKAGE_NAME,
+    async setup(build: PluginBuild) {
+      const stylexRules: { [string]: $ReadOnlyArray<Rule> } = {};
+
+      build.onEnd(async ({ outputFiles }: BuildResult<>) => {
+        const rules: Array<Rule> = Object.values(stylexRules).flat();
+
+        if (rules.length === 0) {
+          return;
+        }
+
+        const collectedCSS = stylexBabelPlugin.processStylexRules(
+          rules,
+          useCSSLayers,
+        );
+        const shouldWriteToDisk =
+          build.initialOptions.write === undefined ||
+          build.initialOptions.write;
+
+        if (shouldWriteToDisk) {
+          await mkdir(path.dirname(generatedCSSFileName), {
+            recursive: true,
+          });
+          await writeFile(generatedCSSFileName, collectedCSS, 'utf8');
+
+          return;
+        }
+
+        if (outputFiles !== undefined) {
+          outputFiles.push({
+            path: '<stdout>',
+            contents: new TextEncoder().encode(collectedCSS),
+            hash: utils.hash(new Date().getTime().toString()),
+            get text() {
+              return collectedCSS;
+            },
+          });
+        }
+      });
+
+      build.onLoad(
+        { filter: STYLEX_PLUGIN_ONLOAD_FILTER },
+        async (args: OnLoadArgs) => {
+          const currFilePath = args.path;
+          const inputCode = await readFile(currFilePath, 'utf8');
+
+          if (
+            !stylexImports.some((importName) => inputCode.includes(importName))
+          ) {
+            // avoid transform if file doesn't have stylex imports
+            // esbuild proceeds to the next callback
+            return;
+          }
+
+          const transformResult = await transformAsync(inputCode, {
+            babelrc: false,
+            filename: currFilePath,
+            presets,
+            plugins: [
+              ...plugins,
+              ...getFlowOrTypeScriptBabelSyntaxPlugins(currFilePath),
+              jsxSyntaxPlugin,
+              stylexBabelPlugin.withOptions({
+                ...options,
+                dev,
+                unstable_moduleResolution,
+              }),
+            ],
+          });
+
+          const loader = getEsbuildLoader(currFilePath);
+
+          if (transformResult === null) {
+            console.warn('StyleX: transformAsync returned null');
+            return { code: inputCode, loader };
+          }
+
+          const { code, metadata } = transformResult;
+
+          if (code === null) {
+            console.warn('StyleX: transformAsync returned null code');
+            return { code: inputCode, loader };
+          }
+
+          if (
+            !dev &&
+            (metadata: $FlowFixMe).stylex !== null &&
+            (metadata: $FlowFixMe).stylex.length > 0
+          ) {
+            stylexRules[args.path] = (metadata: $FlowFixMe).stylex;
+          }
+
+          return {
+            contents: code,
+            loader,
+          };
+        },
+      );
+    },
+  };
+}
+
+function getEsbuildLoader(fileName: string) {
+  if (fileName.endsWith('.tsx')) {
+    return 'tsx';
+  }
+
+  if (fileName.endsWith('.jsx')) {
+    return 'jsx';
+  }
+
+  if (fileName.endsWith('ts')) {
+    return 'ts';
+  }
+
+  return 'js';
+}
+
+function getFlowOrTypeScriptBabelSyntaxPlugins(fileName: string) {
+  if (/\.jsx?/.test(path.extname(fileName))) {
+    return [flowSyntaxPlugin, hermesParserPlugin];
+  }
+
+  return [[typescriptSyntaxPlugin, { isTSX: true }]];
+}
