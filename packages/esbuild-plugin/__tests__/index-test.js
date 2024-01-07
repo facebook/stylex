208f2bf14a96becfe68237900a7a07b450c92caf
Julius <45975811+nedjulius@users.noreply.github.com>
2024-01-07 15:44:24 +0200

    feat: add `esbuild` plugin (#113)
    * Add eslint-plugin package
    
    * Add initial test suite
    
    * Improve CSS handling and tests
    
    * Address some of the comments
    
    * Add babel scripts for jsx, flow and ts
    
    * Add package diff
    
    * Add snapshots and fix nits
    
    * Fix rename bug
    
    * Add esbuild example app
    
    * Fix incorrect loaders
    
    * Update package-lock.json
    
    * Remove unnecessary file
    
    * Fix some nits in example
    
    * Add additional style to text fixture
    
    * Remove ext from import in example
    
    * Address some of the comments
    
    * Update package-lock.json
    
    * Add missing function call and snapshot
    
    * Remove comment
    
    * Pluralize function name
    
    * Add types
    
    * Fix build script
    
    * Add docs
    
    * Fix tabulation
    
    * Retake snapshots
    
    * Format prettier
    
    * Add more missing types
    
    * Run formatter
    
    * Fix formatting
    
    * Lock dependency and fix import
    
    * Disable unused key
    
    * Add docs
    
    * Update package-lock.json
    
    * Update package-lock.json
    
    * Add snapshots
    
    * Address comments

---
+++ packages/esbuild-plugin/__tests__/index-test.js
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ *
+ */
+
+'use strict';
+
+import path from 'path';
+import esbuild from 'esbuild';
+import stylexPlugin from '../src/index';
+
+async function build(options = {}) {
+  const { outputFiles } = await esbuild.build({
+    entryPoints: [path.resolve(__dirname, '__fixtures__/index.js')],
+    external: ['@stylexjs/stylex'],
+    minify: false,
+    bundle: true,
+    write: false,
+    plugins: [stylexPlugin({ useCSSLayers: true, ...options })],
+  });
+
+  return { js: outputFiles[0], css: outputFiles[1] };
+}
+
+describe('esbuild-plugin-stylex', () => {
+  it('extracts and bundles CSS without inject calls, bundles JS', async () => {
+    const { js, css } = await build();
+
+    expect(js.text).toMatchSnapshot();
+
+    expect(css.text).toMatchSnapshot();
+  });
+
+  it('preserves stylex.inject calls and does not extract CSS in development mode', async () => {
+    const { js, css } = await build({ dev: true });
+
+    expect(js.text).toMatchSnapshot();
+
+    expect(css).toBeUndefined();
+  });
+});
