5a1024ba66fcd050797f1c45c0dbd15edbf3f17c
Naman Goel <nmn@users.noreply.github.com>
2024-01-24 03:13:28 -0800

    Fix: Clean up and clarify alias logic

--- apps/nextjs-example/next.config.js
+++ apps/nextjs-example/next.config.js
@@ -8,6 +8,7 @@
  */
 
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const stylexPlugin = require('@stylexjs/nextjs-plugin');
 const babelrc = require('./.babelrc.js');
 const plugins = babelrc.plugins;
@@ -15,13 +16,9 @@ const [_name, options] = plugins.find(
   (plugin) => Array.isArray(plugin) && plugin[0] === '@stylexjs/babel-plugin',
 );
 const rootDir = options.unstable_moduleResolution.rootDir ?? __dirname;
+const aliases = options.aliases ?? undefined;
+const useCSSLayers = options.useCSSLayers ?? undefined;
 
-module.exports = stylexPlugin({
-  rootDir,
-  aliases: {
-    '@/*': ['./*'],
-  },
-  useCSSLayers: true,
-})({
+module.exports = stylexPlugin({ rootDir, aliases, useCSSLayers })({
   transpilePackages: ['@stylexjs/open-props'],
 });
