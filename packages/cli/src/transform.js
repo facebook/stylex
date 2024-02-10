c7502aafe78d4b3aa881ca2bcf9c98678f2eb94e
Jta26 <joel.austinrd@gmail.com>
2024-02-10 02:01:35 -0800

    [cli] very rough working stylex cli

--- packages/cli/src/transform.js
+++ packages/cli/src/transform.js
@@ -8,8 +8,23 @@
  */
 
 import * as babel from '@babel/core';
+import styleXPlugin from '@stylexjs/babel-plugin';
+import type { Rule } from '@stylexjs/babel-plugin';
 
-export async function transformFile(filename: string): Promise<> {
-  const result = await babel.transformFileAsync(filename, {});
+export async function transformFile(
+  dir: string,
+  fileName: string,
+): Promise<[?string, Array<Rule>]> {
+  const result = await babel.transformFileAsync(fileName, {
+    plugins: [styleXPlugin],
+  });
+  // $FlowFixMe
   const { code, metadata } = result;
+  // $FlowFixMe
+  const styleXRules: Array<Rule> = metadata.stylex;
+  return [code, styleXRules];
+}
+
+export async function compileRules(rules: Array<Rule>): Promise<string> {
+  return styleXPlugin.processStylexRules(rules);
 }
