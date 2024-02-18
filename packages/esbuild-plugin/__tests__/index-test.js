423c8396be4c1c1d0174ab186acb550bf2d87871
nedjulius <julius.nedzinskas@gmail.com>
2024-02-18 12:46:54 -0500

    Add additional test for TS

--- packages/esbuild-plugin/__tests__/index-test.js
+++ packages/esbuild-plugin/__tests__/index-test.js
@@ -14,13 +14,22 @@ import esbuild from 'esbuild';
 import stylexPlugin from '../src/index';
 
 async function build(options = {}) {
+  const { isTS, ...rest } = options;
+  const tsConfigPath = path.resolve(
+    __dirname,
+    '__fixtures__/test-tsconfig.json',
+  );
+
   const { outputFiles } = await esbuild.build({
-    entryPoints: [path.resolve(__dirname, '__fixtures__/index.js')],
+    entryPoints: [
+      path.resolve(__dirname, `__fixtures__/index.${isTS ? 'ts' : 'js'}`),
+    ],
     external: ['@stylexjs/stylex'],
+    tsconfig: isTS ? tsConfigPath : undefined,
     minify: false,
     bundle: true,
     write: false,
-    plugins: [stylexPlugin({ useCSSLayers: true, ...options })],
+    plugins: [stylexPlugin({ useCSSLayers: true, ...rest })],
   });
 
   return { js: outputFiles[0], css: outputFiles[1] };
@@ -29,6 +38,14 @@ async function build(options = {}) {
 describe('esbuild-plugin-stylex', () => {
   it('extracts and bundles CSS without inject calls, bundles JS', async () => {
     const { js, css } = await build();
+
+    expect(js.text).toMatchSnapshot();
+
+    expect(css.text).toMatchSnapshot();
+  });
+
+  it('extracts and bundles CSS without inject calls, bundles and transforms TS', async () => {
+    const { js, css } = await build({ isTS: true });
 
     expect(js.text).toMatchSnapshot();
 
