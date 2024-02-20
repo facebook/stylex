40e177cf59019e90401b67ac0221b75ce3b8c220
Fred Rivett <fred@fredrivett.com>
2024-02-20 12:44:45 +0000

    Add babel plugin options comment to app examples
    I spent half a day implementing stylex into esbuild trying to work out
    why my stylex variables were getting stripped out unless I had a
    `console.log` logging them. It turns out there's an option for the
    [babel plugin
    options](https://stylexjs.com/docs/api/configuration/babel-plugin/) of
    `treeshakeCompensation` which solved the issue.
    
    It was not obvious to me that the object here was being passed to the
    babel plugin behind the scenes, which I only discovered by digging
    through the source code. Hopefully this comment helps others gain a
    better understanding of how the `stylexPlugin` works under the hood.

--- apps/webpack-example/webpack.config.js
+++ apps/webpack-example/webpack.config.js
@@ -30,6 +30,8 @@ const config = (env, argv) => ({
     ],
   },
   plugins: [
+    // See all options in the babel plugin configuration docs:
+    // https://stylexjs.com/docs/api/configuration/babel-plugin/
     new StylexPlugin({
       filename: 'styles.[contenthash].css',
       // get webpack mode and set value for dev
