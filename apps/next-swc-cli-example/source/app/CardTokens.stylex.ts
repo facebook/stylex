a94698a0b43772d838a35e40ce144b1839af48c9
Jta26 <joel.austinrd@gmail.com>
2024-02-26 16:44:51 -0800

    [cli] next swc example

---
+++ apps/next-swc-cli-example/source/app/CardTokens.stylex.ts
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+import * as stylex from '@stylexjs/stylex';
+
+export const tokens = stylex.defineVars({
+  arrowTransform: 'translateX(0)',
+});
