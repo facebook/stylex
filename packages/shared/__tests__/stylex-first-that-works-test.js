240fbe537f327cf1cceb67cf27213ad48a92bfad
Naman Goel <nmn@users.noreply.github.com>
2024-06-30 06:55:48 -0700

    feat: allow using firstThatWorks for variable fallbacks

--- packages/shared/__tests__/stylex-first-that-works-test.js
+++ packages/shared/__tests__/stylex-first-that-works-test.js
@@ -13,4 +13,21 @@ describe('stylex-first-that-works test', () => {
   test('reverses simple array of values', () => {
     expect(firstThatWorks('a', 'b', 'c')).toEqual(['c', 'b', 'a']);
   });
+  test('creates fallbacks for variables', () => {
+    expect(firstThatWorks('var(--accent)', 'blue')).toEqual(
+      'var(--accent, blue)',
+    );
+  });
+  test('Allow variables to be fallbacks too', () => {
+    expect(
+      firstThatWorks(
+        'color-mix(in srgb, currentColor 20%, transparent)',
+        'var(--accent)',
+        'blue',
+      ),
+    ).toEqual([
+      'var(--accent, blue)',
+      'color-mix(in srgb, currentColor 20%, transparent)',
+    ]);
+  });
 });
