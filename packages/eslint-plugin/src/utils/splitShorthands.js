d750e402ce688f9bed00c2071627408148baaef1
Melissa Liu <mellyeliu@meta.com>
2024-08-20 16:50:17 -0700

    handle duplicated values properly

--- packages/eslint-plugin/src/utils/splitShorthands.js
+++ packages/eslint-plugin/src/utils/splitShorthands.js
@@ -12,6 +12,58 @@ import cssExpand from 'css-shorthand-expand';
 
 export const CANNOT_FIX = 'CANNOT_FIX';
 
+export const createSpecificShorthandTransformer = (property) => {
+  return (rawValue: number | string, allowImportant: boolean = false, _preferInline: boolean = false) => {
+    return splitSpecificShorthands(property, rawValue.toString(), allowImportant);
+  };
+};
+
+export const createDirectionalTransformer = (baseProperty, blockSuffix, inlineSuffix) => {
+  return (rawValue: number | string, allowImportant: boolean = false, preferInline: boolean = false) => {
+    const splitValues = splitDirectionalShorthands(rawValue, allowImportant);
+    const [top, right = top, bottom = top, left = right] = splitValues;
+
+    if (splitValues.length === 1) {
+      return [[`${baseProperty}`, top]];
+    }
+
+    if (splitValues.length === 2) {
+      return [
+        [`${baseProperty}${blockSuffix}`, top],
+        [`${baseProperty}${inlineSuffix}`, right],
+      ];
+    }
+
+    return preferInline
+      ? [
+          [`${baseProperty}Top`, top],
+          [`${baseProperty}${inlineSuffix}End`, right],
+          [`${baseProperty}Bottom`, bottom],
+          [`${baseProperty}${inlineSuffix}Start`, left],
+        ]
+      : [
+          [`${baseProperty}Top`, top],
+          [`${baseProperty}Right`, right],
+          [`${baseProperty}Bottom`, bottom],
+          [`${baseProperty}Left`, left],
+        ];
+  };
+};
+
+export const createBlockInlineTransformer = (baseProperty, blockSuffix, inlineSuffix) => {
+  return (rawValue: number | string, allowImportant: boolean = false) => {
+    const splitValues = splitDirectionalShorthands(rawValue, allowImportant);
+    if (splitValues.length === 1) {
+      return [[`${baseProperty}${blockSuffix}${inlineSuffix}`, splitValues[0]]];
+    }
+    const [start, end = start] = splitValues;
+    return [
+      [`${baseProperty}${blockSuffix}Start`, start],
+      [`${baseProperty}${blockSuffix}End`, end],
+    ];
+  };
+};
+
 function printNode(node: PostCSSValueASTNode): string {
   switch (node.type) {
     case 'word':
@@ -28,7 +80,7 @@ const toCamelCase = (str: string) => {
   return str.replace(/-([a-z])/g, (match, letter) => letter.toUpperCase());
 };
 
-/* The css-shorthands-expand library does not handle spaces within variables like `rgb(0, 0, 0) or var(-test-var, 0) properly. 
+/* The css-shorthands-expand library does not handle spaces within variables like `rgb(0, 0, 0) or var(-test-var, 0) properly.
 In cases with simple spaces between comma-separated parameters, we can preprocess the values by stripping the spaces.
 If there are still spaces remaining, such as in edge cases involving `calc()` or gradient values, we won't provide an auto-fix. */
 function processWhitespacesinFunctions(str: string) {
@@ -59,7 +111,7 @@ function processWhitespacesinFunctions(str: string) {
   };
 }
 
-/* The css-shorthands-expand library does not handle spaces within variables like `rgb(0, 0, 0) or var(-test-var, 0) properly. 
+/* The css-shorthands-expand library does not handle spaces within variables like `rgb(0, 0, 0) or var(-test-var, 0) properly.
 After stripping the spaces, let's post-process the values to add back the missing whitespaces between parentheses after a comma */
 function addSpacesAfterCommasInParentheses(str: string) {
   return str.replace(/\(([^)]+)\)/g, (match, p1) => {
@@ -141,6 +193,7 @@ export function splitDirectionalShorthands(
 
   if (typeof str === 'number') {
     // if originally a number, let's preserve that here
+    console.log('nodesNUMBER', nodes)
     const processedNodes = nodes.map(parseFloat);
     return processedNodes;
   }
@@ -151,6 +204,13 @@ export function splitDirectionalShorthands(
     allowImportant
   ) {
     return nodes.slice(0, nodes.length - 1).map((node) => node + ' !important');
+  }
+
+  console.log('nodes', nodes);
+
+  if (nodes.length > 1 && new Set(nodes).size === 1) {
+    // If all values are the same, no need to expand
+    return [nodes[0]];
   }
 
   return nodes;
