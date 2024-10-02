a882b55f13efdc0d72ceab3f23913578e44b595a
Naman Goel <nmn@users.noreply.github.com>
2024-10-02 13:49:45 -0400

    chore: migrate to algolia search

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -23,7 +23,6 @@ const config = {
   // GitHub pages deployment config.
   organizationName: 'facebook',
   projectName: 'stylex',
-  plugins: ['@orama/plugin-docusaurus'],
 
   presets: [
     [
@@ -54,6 +53,38 @@ const config = {
       },
       prism: {
         theme: require('prism-react-renderer/themes/dracula'),
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: '63XRZ0SHHM',
+
+        // Public API key: it is safe to commit it
+        apiKey: '1e20035f8c22349860eb8d8440eb7435',
+
+        indexName: 'stylex_docs',
+
+        // Optional: see doc section below
+        // contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+        //   from: '/docs/', // or as RegExp: /\/docs\//
+        //   to: '/',
+        // },
+
+        // Optional: Algolia search parameters
+        // searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        // searchPagePath: 'search',
+
+        // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+        // insights: false,
+
+        //... other Algolia params
       },
       navbar: {
         title: '',
