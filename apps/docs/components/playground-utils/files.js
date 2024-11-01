fbedfe7e0e729e8e6c821f7221de6dcf68fffd8e
brian <bri4n.huang@gmail.com>
2024-10-31 22:02:22 -0700

    feat: adjust build setup to use vite

--- apps/docs/components/playground-utils/files.js
+++ apps/docs/components/playground-utils/files.js
@@ -8,83 +8,118 @@
  */
 
 export const files = {
-  '.babelrc.js': {
+  'generateCSS.js': {
     file: {
       contents: `
-module.exports = {
-  presets: ["@babel/preset-react"],
-  plugins: [
-    "transform-node-env-inline",
-    [
-      "@stylexjs/babel-plugin",
-      {
-        dev: false,
-        stylexSheetName: "<>",
-        genConditionalClasses: true,
-        unstable_moduleResolution: {
-          type: "commonJS",
-          rootDir: "/",
+const fs = require("fs/promises");
+const { transformAsync } = require("@babel/core");
+const stylexBabelPlugin = require("@stylexjs/babel-plugin");
+const flowSyntaxPlugin = require("@babel/plugin-syntax-flow");
+const jsxSyntaxPlugin = require("@babel/plugin-syntax-jsx");
+const path = require("path");
+const { mkdirp } = require("mkdirp");
+
+async function transformFile(filePath) {
+  const code = await fs.readFile(filePath, "utf8");
+  const result = await transformAsync(code, {
+    filename: filePath,
+    plugins: [
+      flowSyntaxPlugin,
+      jsxSyntaxPlugin,
+      [
+        stylexBabelPlugin,
+        {
+          dev: false,
+          test: false,
+          stylexSheetName: "<>",
+          genConditionalClasses: true,
+          unstable_moduleResolution: {
+            type: "commonJS",
+            rootDir: path.join(__dirname),
+          },
         },
-      },
+      ],
     ],
-  ],
-};
-      
-`,
+    sourceType: "unambiguous",
+    babelrc: false,
+  });
+  return result.metadata.stylex;
+}
+
+async function getAllFilesOfType(folder, type) {
+  const contents = await fs.readdir(folder, { withFileTypes: true });
+
+  const files = await Promise.all(
+    contents.map(async (dirent) => {
+      const subPath = path.join(folder, dirent.name);
+      if (dirent.isDirectory()) {
+        return await getAllFilesOfType(subPath, type);
+      }
+      if (dirent.name.endsWith(type)) {
+        return subPath;
+      }
+      return null;
+    })
+  );
+
+  return files.flat().filter(Boolean);
+}
+
+async function genSheet() {
+  const src = await getAllFilesOfType(path.join(__dirname, "src"), ".jsx");
+  const ruleSets = await Promise.all(src.map(transformFile));
+  const generatedCSS = stylexBabelPlugin.processStylexRules(ruleSets.flat());
+  const outputDir = path.join(__dirname, "src");
+  const cssPath = path.join(outputDir, "stylex.css");
+
+  await mkdirp(outputDir);
+  await fs.writeFile(cssPath, generatedCSS);
+
+  console.log("Successfully generated CSS.");
+}
+
+genSheet();
+      `,
     },
   },
   'index.html': {
     file: {
       contents: `
 <!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>Stylex Playground</title>
-  <link rel="stylesheet" href="./stylex.css">
-</head>
-<body>
-  <h1>Loaded HTML from webcontainer!</h1>
-  <div id="root"></div>
-  <script src="./bundle.js"></script>
-</body>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Stylex Playground</title>
+  </head>
+  <body>
+    <h1 style="color: blue">Loaded HTML from webcontainer!</h1>
+    <div id="root"></div>
+    <script type="module" src="./src/main.jsx"></script>
+  </body>
 </html>
       `,
     },
   },
-  'rollup.config.mjs': {
+  'vite.config.js': {
     file: {
       contents: `
-import babel from "@rollup/plugin-babel";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import serve from "rollup-plugin-serve";
-import stylex from "@stylexjs/rollup-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-const extensions = [".js", ".jsx", "ts", "tsx"];
-
-export default {
-  extensions,
-  input: "./input.js",
-  output: {
-    file: "./bundle.js",
-    format: "umd",
-  },
+export default defineConfig({
   plugins: [
-    stylex({ fileName: "stylex.css" }),
-    babel({
-      babelHelpers: "bundled",
-      extensions,
-      configFile: "./.babelrc.js",
-    }),
-    resolve({ extensions }),
-    commonjs(),
-    serve({
-      contentBase: "./",
-      port: 3111,
+    react({
+      babel: {
+        presets: ["@babel/preset-react"],
+        plugins: ["@stylexjs/babel-plugin"],
+      },
     }),
   ],
-};
+  server: {
+    port: 3111,
+  },
+});
       `,
     },
   },
@@ -97,8 +132,7 @@ export default {
   "description": "Playground using WebContainers",
   "main": "index.js",
   "scripts": {
-    "build": "babel ./input.js -o ./output.js",
-    "start": "rollup --config ./rollup.config.mjs --watch"
+    "start": "node generateCSS.js && vite dev"
   },
   "dependencies": {
     "@babel/cli": "latest",
@@ -108,52 +142,46 @@ export default {
     "@babel/plugin-syntax-typescript": "latest",
     "@babel/preset-env": "^7.23.5",
     "@babel/preset-react": "^7.23.3",
-    "@rollup/plugin-babel": "6.0.4",
-    "@rollup/plugin-commonjs": "25.0.7",
-    "@rollup/plugin-node-resolve": "15.2.3",
-    "@rollup/plugin-typescript": "^11.1.5",
-    "@stylexjs/babel-plugin": "0.7.5",
-    "@stylexjs/rollup-plugin": "0.7.5",
-    "@stylexjs/stylex": "0.7.5",
+    "@stylexjs/babel-plugin": "^0.8.0",
+    "@stylexjs/stylex": "^0.8.0",
     "babel-plugin-transform-node-env-inline": "^0.4.3",
     "react": "*",
     "react-dom": "*",
-    "rollup": "4.6.1",
-    "rollup-plugin-serve": "3.0.0"
+    "vite": "^5.4.10",
+    "@vitejs/plugin-react": "^4.3.3",
+    "mkdirp": "^3.0.1"
   }
 }
-`,
-    },
-  },
-  'input.js': {
-    file: {
-      contents: `
-import * as React from "react";
-import { createRoot } from 'react-dom/client';
-import Card from "./src/app";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(React.createElement(Card, { em: true }, "Hello World!"));
       `,
     },
   },
   src: {
     directory: {
+      'main.jsx': {
+        file: {
+          contents: `
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import Card from "./app.jsx";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Card em={true}>Hello World!</Card>);
+          `,
+        },
+      },
       'app.jsx': {
         file: {
           contents: `
-import * as React from 'react';
+import * as React from "react";
 import * as stylex from "@stylexjs/stylex";
+import "./stylex.css";
 
-export default function Card({ children, em = false, ...props }) {
-  return React.createElement(
-    "div", 
-    {
-      ...props,
-      ...stylex.props(styles.base, em && styles.emphasise)
-    },
-    children,
+export default function Card({ children, em = false, props }) {
+  return (
+    <div {...props} {...stylex.props(styles.base, em && styles.emphasise)}>
+      {children}
+    </div>
   );
 }
 
@@ -165,16 +193,16 @@ const styles = stylex.create({
     borderStyle: "none",
     boxSize: "border-box",
     color: "white",
-    marginInline: 'auto',
+    marginInline: "auto",
     paddingBlock: 4,
     paddingInline: 8,
-    width: '95%',
+    width: "95%",
   },
   emphasise: {
     transform: "rotate(-2deg)",
-  }
+  },
 });
-          `,
+`,
         },
       },
     },
