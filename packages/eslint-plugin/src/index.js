ec136027e3d292490747789f1ae1c0d9c35dff39
Samantha <40647818+Samantha-Zhan@users.noreply.github.com>
2024-11-11 12:11:39 -0800

    feat: add no-unused eslint rule to find unused styles (#767)
    * feat: add no-unused eslint rule
    * Addressed comments; added tests: named exports, indirect style invoke; added features: non-default export, used style as return, full import pattern support

--- packages/eslint-plugin/src/index.js
+++ packages/eslint-plugin/src/index.js
@@ -10,15 +10,18 @@
 import validStyles from './stylex-valid-styles';
 import sortKeys from './stylex-sort-keys';
 import validShorthands from './stylex-valid-shorthands';
+import noUnused from './stylex-no-unused';
 
 const rules: {
   'valid-styles': typeof validStyles,
   'sort-keys': typeof sortKeys,
   'valid-shorthands': typeof validShorthands,
+  'no-unused': typeof noUnused,
 } = {
   'valid-styles': validStyles,
   'sort-keys': sortKeys,
   'valid-shorthands': validShorthands,
+  'no-unused': noUnused,
 };
 
 export { rules };
