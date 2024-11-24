05e6f89f84302b1cf367653da6fe854fb92c3243
Tsubasa Sakai <javascripter@tsukkun.net>
2024-11-24 13:39:04 +0000

    feat: add postcss-plugin

---
+++ apps/nextjs-example/postcss.config.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ *
+ */
+
+module.exports = {
+  plugins: {
+    '@stylexjs/postcss-plugin': {
+      include: [
+        'app/**/*.{js,jsx,ts,tsx}',
+        'components/**/*.{js,jsx,ts,tsx}',
+        // TODO: Figure out a better way to write this
+        './node_modules/@stylexjs/open-props/lib/**/*.{js,jsx,ts,tsx}',
+      ],
+      useCSSLayers: true,
+    },
+    autoprefixer: {},
+  },
+};
