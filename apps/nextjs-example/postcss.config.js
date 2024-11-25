378c80e5811912511f94a1ad50b85419eb78f687
Tsubasa Sakai <javascripter@tsukkun.net>
2024-11-25 23:01:12 +0000

    fix: do not rely on internal exports path

--- apps/nextjs-example/postcss.config.js
+++ apps/nextjs-example/postcss.config.js
@@ -7,14 +7,48 @@
  *
  */
 
+const fs = require('fs');
+const path = require('path');
+
+const projectRoot = __dirname;
+const monorepoRoot = path.join(projectRoot, '../../');
+
+function getPackageIncludePaths(packageName, nodeModulePaths) {
+  let packagePath = null;
+
+  for (const nodeModulePath of nodeModulePaths) {
+    const packageJsonPath = path.resolve(
+      nodeModulePath,
+      packageName,
+      'package.json',
+    );
+    if (fs.existsSync(packageJsonPath)) {
+      packagePath = path.dirname(packageJsonPath);
+      break;
+    }
+  }
+  if (!packagePath) {
+    throw new Error(`Could not find package ${packageName}`);
+  }
+
+  return [
+    path.join(packagePath, '**/*.{js,mjs}'),
+    '!' + path.join(packagePath, 'node_modules/**/*.{js,mjs}'),
+  ];
+}
+
+const openPropsIncludePaths = getPackageIncludePaths('@stylexjs/open-props', [
+  path.join(projectRoot, 'node_modules'),
+  path.join(monorepoRoot, 'node_modules'),
+]);
+
 module.exports = {
   plugins: {
     '@stylexjs/postcss-plugin': {
       include: [
         'app/**/*.{js,jsx,ts,tsx}',
         'components/**/*.{js,jsx,ts,tsx}',
-        // TODO: Figure out a better way to write this
-        './node_modules/@stylexjs/open-props/lib/**/*.{js,mjs}',
+        ...openPropsIncludePaths,
       ],
       useCSSLayers: true,
     },
