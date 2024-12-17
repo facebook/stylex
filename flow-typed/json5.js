578c94349f51b55d1a1b4e446d87d50ca6c145b9
p0nch000 <A01198400@tec.mx>
2024-12-17 20:08:50 +0100

    Fixed flow and prettier tests failing

---
+++ flow-typed/json5.js
@@ -0,0 +1,25 @@
+// @flow
+type TSConfig = {
+  compilerOptions?: {
+    baseUrl?: string,
+    paths?: { [key: string]: Array<string> },
+  },
+};
+
+type DenoConfig = {
+  imports?: { [key: string]: string | Array<string> },
+};
+
+type PackageJSON = {
+  name?: string,
+  imports?: { [key: string]: string | Array<string> },
+};
+
+type ConfigType = TSConfig | DenoConfig | PackageJSON;
+
+declare module 'json5' {
+  declare module.exports: {
+    parse: (input: string) => mixed,
+    stringify: (value: mixed, replacer?: ?Function | ?Array<mixed>, space?: string | number) => string,
+  };
+} 
