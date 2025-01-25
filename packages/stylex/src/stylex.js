02a93a056370a3f9df0f99e611d8e6d481ed7387
Naman Goel <nmn@users.noreply.github.com>
2025-01-24 21:13:45 -0800

    feat: exported return types of 'stylex.types.*' fns (#867)
    * feat: exported return types of 'stylex.types.*' fns
    * chore: move type Types to stylex.Types.*

--- packages/stylex/src/stylex.js
+++ packages/stylex/src/stylex.js
@@ -21,7 +21,6 @@ import type {
   MapNamespaces,
 } from './StyleXTypes';
 import type {
-  ValueWithDefault,
   Angle,
   Color,
   Url,
@@ -36,6 +35,8 @@ import type {
   TransformFunction,
   TransformList,
 } from './VarTypes';
+import type { ValueWithDefault } from './util-types';
+export * as Types from './VarTypes';
 
 export type {
   VarGroup,
