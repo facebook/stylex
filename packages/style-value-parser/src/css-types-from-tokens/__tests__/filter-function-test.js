5932fba2ec4c50c2b24e8892b55a0d9cdff58b71
Naman Goel <nmn@users.noreply.github.com>
2025-02-10 22:38:13 -0800

    chore: add more tests. Next to fix them all

---
+++ packages/style-value-parser/src/css-types-from-tokens/__tests__/filter-function-test.js
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict
+ */
+
+import {
+  FilterFunction,
+  BlurFilterFunction,
+  BrightnessFilterFunction,
+  ContrastFilterFunction,
+  GrayscaleFilterFunction,
+  HueRotateFilterFunction,
+  InverFilterFunction,
+  OpacityFilterFunction,
+  SaturateFilterFunction,
+  SepiaFilterFunction,
+} from '../filter-function';
+import { Length } from '../length';
+import { Angle } from '../angle';
+
+describe.skip('Test CSS Type: <filter-function>', () => {
+  test('parses blur filter', () => {
+    expect(FilterFunction.parse.parse('blur(5px)')).toEqual(
+      new BlurFilterFunction(new Length(5, 'px')),
+    );
+  });
+
+  test('parses brightness filter', () => {
+    expect(FilterFunction.parse.parse('brightness(150%)')).toEqual(
+      new BrightnessFilterFunction(1.5),
+    );
+  });
+
+  test('parses contrast filter', () => {
+    expect(FilterFunction.parse.parse('contrast(200%)')).toEqual(
+      new ContrastFilterFunction(2),
+    );
+  });
+
+  test('parses grayscale filter', () => {
+    expect(FilterFunction.parse.parse('grayscale(50%)')).toEqual(
+      new GrayscaleFilterFunction(0.5),
+    );
+  });
+
+  test('parses hue-rotate filter', () => {
+    expect(FilterFunction.parse.parse('hue-rotate(90deg)')).toEqual(
+      new HueRotateFilterFunction(new Angle(90, 'deg')),
+    );
+  });
+
+  test('parses invert filter', () => {
+    expect(FilterFunction.parse.parse('invert(100%)')).toEqual(
+      new InverFilterFunction(1),
+    );
+  });
+
+  test('parses opacity filter', () => {
+    expect(FilterFunction.parse.parse('opacity(75%)')).toEqual(
+      new OpacityFilterFunction(0.75),
+    );
+  });
+
+  test('parses saturate filter', () => {
+    expect(FilterFunction.parse.parse('saturate(120%)')).toEqual(
+      new SaturateFilterFunction(1.2),
+    );
+  });
+
+  test('parses sepia filter', () => {
+    expect(FilterFunction.parse.parse('sepia(30%)')).toEqual(
+      new SepiaFilterFunction(0.3),
+    );
+  });
+
+  // test('rejects invalid filter functions', () => {
+  //   expect(() => FilterFunction.parse.parseToEnd('invalid()')).toThrow();
+  //   expect(() => FilterFunction.parse.parseToEnd('blur()')).toThrow();
+  //   expect(() => FilterFunction.parse.parseToEnd('brightness(abc)')).toThrow();
+  // });
+});
---
+++ packages/style-value-parser/src/css-types-from-tokens/__tests__/length-test.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict
+ */
+
+import { Length } from '../length';
+
+describe('Test CSS Type: <length>', () => {
+  test('parses font-based units', () => {
+    expect(Length.parse.parse('10ch')).toEqual(new Length(10, 'ch'));
+    expect(Length.parse.parse('2em')).toEqual(new Length(2, 'em'));
+    expect(Length.parse.parse('1.5ex')).toEqual(new Length(1.5, 'ex'));
+    expect(Length.parse.parse('3ic')).toEqual(new Length(3, 'ic'));
+    expect(Length.parse.parse('1.2lh')).toEqual(new Length(1.2, 'lh'));
+    expect(Length.parse.parse('2rem')).toEqual(new Length(2, 'rem'));
+    expect(Length.parse.parse('1rlh')).toEqual(new Length(1, 'rlh'));
+  });
+
+  test('parses viewport-based units', () => {
+    expect(Length.parse.parse('50vh')).toEqual(new Length(50, 'vh'));
+    expect(Length.parse.parse('100vw')).toEqual(new Length(100, 'vw'));
+    expect(Length.parse.parse('80svh')).toEqual(new Length(80, 'svh'));
+    expect(Length.parse.parse('90lvw')).toEqual(new Length(90, 'lvw'));
+    expect(Length.parse.parse('70dvh')).toEqual(new Length(70, 'dvh'));
+    expect(Length.parse.parse('60vmin')).toEqual(new Length(60, 'vmin'));
+    expect(Length.parse.parse('85vmax')).toEqual(new Length(85, 'vmax'));
+  });
+
+  test('parses container-based units', () => {
+    expect(Length.parse.parse('30cqw')).toEqual(new Length(30, 'cqw'));
+    expect(Length.parse.parse('40cqi')).toEqual(new Length(40, 'cqi'));
+    expect(Length.parse.parse('50cqh')).toEqual(new Length(50, 'cqh'));
+    expect(Length.parse.parse('60cqb')).toEqual(new Length(60, 'cqb'));
+    expect(Length.parse.parse('45cqmin')).toEqual(new Length(45, 'cqmin'));
+    expect(Length.parse.parse('75cqmax')).toEqual(new Length(75, 'cqmax'));
+  });
+
+  test('parses absolute units', () => {
+    expect(Length.parse.parse('16px')).toEqual(new Length(16, 'px'));
+    expect(Length.parse.parse('2cm')).toEqual(new Length(2, 'cm'));
+    expect(Length.parse.parse('10mm')).toEqual(new Length(10, 'mm'));
+    expect(Length.parse.parse('1in')).toEqual(new Length(1, 'in'));
+    expect(Length.parse.parse('12pt')).toEqual(new Length(12, 'pt'));
+  });
+
+  // test('rejects invalid units', () => {
+  //   expect(() => Length.parse.parseToEnd('10abc')).toThrow();
+  //   expect(() => Length.parse.parseToEnd('20pc')).toThrow();
+  //   expect(() => Length.parse.parseToEnd('30')).toThrow();
+  //   expect(() => Length.parse.parseToEnd('xyz')).toThrow();
+  // });
+});
