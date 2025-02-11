489075b21c4823e7564fbb0ef4d51e299ae82589
Naman Goel <nmn@users.noreply.github.com>
2025-02-10 22:38:13 -0800

    chore: add more tests. Next to fix them all

---
+++ packages/style-value-parser/src/css-types-from-tokens/__tests__/color-test.js
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict
+ */
+
+import { Color, NamedColor, HashColor, Rgb, Rgba } from '../color';
+
+describe('Test CSS Type: <color>', () => {
+  test('parses named colors', () => {
+    expect(Color.parse.parse('red')).toEqual(new NamedColor('red'));
+    expect(Color.parse.parse('blue')).toEqual(new NamedColor('blue'));
+    expect(Color.parse.parse('green')).toEqual(new NamedColor('green'));
+    expect(Color.parse.parse('transparent')).toEqual(
+      new NamedColor('transparent'),
+    );
+  });
+
+  test('parses hash colors', () => {
+    expect(Color.parse.parse('#ff0000')).toEqual(new HashColor('ff0000'));
+    expect(Color.parse.parse('#00ff00')).toEqual(new HashColor('00ff00'));
+    expect(Color.parse.parse('#0000ff')).toEqual(new HashColor('0000ff'));
+    expect(Color.parse.parse('#ffffff')).toEqual(new HashColor('ffffff'));
+  });
+
+  test.skip('parses RGB values', () => {
+    expect(Color.parse.parse('rgb(255, 0, 0)')).toEqual(new Rgb(255, 0, 0));
+    expect(Color.parse.parse('rgb(0, 255, 0)')).toEqual(new Rgb(0, 255, 0));
+    expect(Color.parse.parse('rgb(0, 0, 255)')).toEqual(new Rgb(0, 0, 255));
+  });
+
+  test.skip('parses RGBA values', () => {
+    expect(Color.parse.parse('rgba(255, 0, 0, 0.5)')).toEqual(
+      new Rgba(255, 0, 0, 0.5),
+    );
+    expect(Color.parse.parse('rgba(0, 255, 0, 0.5)')).toEqual(
+      new Rgba(0, 255, 0, 0.5),
+    );
+    expect(Color.parse.parse('rgba(0, 0, 255, 0.5)')).toEqual(
+      new Rgba(0, 0, 255, 0.5),
+    );
+  });
+
+  test.skip('rejects invalid colors', () => {
+    expect(() => Color.parse.parseToEnd('invalid')).toThrow();
+    expect(() => Color.parse.parseToEnd('#gggggg')).toThrow();
+    expect(() => Color.parse.parseToEnd('rgb(256, 0, 0)')).toThrow();
+  });
+});
