5e46aa810fb37f88b1e676308d1fa523c5a7c1a2
Naman Goel <nmn@users.noreply.github.com>
2025-02-13 02:31:20 -0800

    chore: all known core bugs fixed

--- packages/style-value-parser/src/css-types-from-tokens/__tests__/position-test.js
+++ packages/style-value-parser/src/css-types-from-tokens/__tests__/position-test.js
@@ -12,6 +12,25 @@ import { Length } from '../length';
 import { Percentage } from '../common-types';
 
 describe('Test CSS Type: <position>', () => {
+  test('parses single keywords', () => {
+    expect(Position.parse.parse('left')).toEqual(
+      new Position('left', undefined),
+    );
+    expect(Position.parse.parse('right')).toEqual(
+      new Position('right', undefined),
+    );
+    expect(Position.parse.parse('center')).toEqual(
+      new Position('center', undefined),
+    );
+    expect(Position.parse.parse('top')).toEqual(new Position(undefined, 'top'));
+    expect(Position.parse.parse('bottom')).toEqual(
+      new Position(undefined, 'bottom'),
+    );
+    expect(Position.parse.parse('center')).toEqual(
+      new Position('center', undefined),
+    );
+  });
+
   test('parses keyword combinations', () => {
     expect(Position.parse.parse('left top')).toEqual(
       new Position('left', 'top'),
@@ -37,42 +56,42 @@ describe('Test CSS Type: <position>', () => {
     expect(Position.parse.parse('right 20px')).toEqual(
       new Position(['right', new Length(20, 'px')]),
     );
-    // expect(Position.parse.parse('50% top')).toEqual(
-    //   new Position(undefined, ['top', new Percentage(50)]),
-    // );
-    // expect(Position.parse.parse('30px bottom')).toEqual(
-    //   new Position(undefined, ['bottom', new Length(30, 'px')]),
-    // );
+    expect(Position.parse.parse('50% top')).toEqual(
+      new Position(new Percentage(50), 'top'),
+    );
+    expect(Position.parse.parse('30px bottom')).toEqual(
+      new Position(new Length(30, 'px'), 'bottom'),
+    );
   });
 
-  // test('parses length-percentage combinations', () => {
-  //   expect(Position.parse.parse('50% 50%')).toEqual(
-  //     new Position(new Percentage(50), new Percentage(50)),
-  //   );
-  //   expect(Position.parse.parse('20px 30px')).toEqual(
-  //     new Position(new Length(20, 'px'), new Length(30, 'px')),
-  //   );
-  //   expect(Position.parse.parse('25% 40px')).toEqual(
-  //     new Position(new Percentage(25), new Length(40, 'px')),
-  //   );
-  // });
+  test('parses length-percentage combinations', () => {
+    expect(Position.parse.parse('50% 50%')).toEqual(
+      new Position(new Percentage(50), new Percentage(50)),
+    );
+    expect(Position.parse.parse('20px 30px')).toEqual(
+      new Position(new Length(20, 'px'), new Length(30, 'px')),
+    );
+    expect(Position.parse.parse('25% 40px')).toEqual(
+      new Position(new Percentage(25), new Length(40, 'px')),
+    );
+  });
 
-  // test('parses keyword with offset', () => {
-  //   expect(Position.parse.parse('left 20% top 30%')).toEqual(
-  //     new Position(['left', new Percentage(20)], ['top', new Percentage(30)]),
-  //   );
-  //   expect(Position.parse.parse('right 10px bottom 15px')).toEqual(
-  //     new Position(
-  //       ['right', new Length(10, 'px')],
-  //       ['bottom', new Length(15, 'px')],
-  //     ),
-  //   );
-  // });
+  test('parses keyword with offset', () => {
+    expect(Position.parse.parse('left 20% top 30%')).toEqual(
+      new Position(['left', new Percentage(20)], ['top', new Percentage(30)]),
+    );
+    expect(Position.parse.parse('right 10px bottom 15px')).toEqual(
+      new Position(
+        ['right', new Length(10, 'px')],
+        ['bottom', new Length(15, 'px')],
+      ),
+    );
+  });
 
-  // test('rejects invalid positions', () => {
-  //   expect(() => Position.parse.parseToEnd('invalid')).toThrow();
-  //   expect(() => Position.parse.parseToEnd('left left')).toThrow();
-  //   expect(() => Position.parse.parseToEnd('top right bottom')).toThrow();
-  //   expect(() => Position.parse.parseToEnd('20')).toThrow();
-  // });
+  test('rejects invalid positions', () => {
+    expect(() => Position.parse.parseToEnd('invalid')).toThrow();
+    expect(() => Position.parse.parseToEnd('left left')).toThrow();
+    expect(() => Position.parse.parseToEnd('top right bottom')).toThrow();
+    expect(() => Position.parse.parseToEnd('20')).toThrow();
+  });
 });
