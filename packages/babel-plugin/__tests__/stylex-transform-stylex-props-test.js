582740db02f34bda6f908196afc276402180d001
Melissa Liu <mellyeliu@meta.com>
2025-03-05 18:07:45 -0800

    fix: add stylex transform tests for debug mode

--- packages/babel-plugin/__tests__/stylex-transform-stylex-props-test.js
+++ packages/babel-plugin/__tests__/stylex-transform-stylex-props-test.js
@@ -1435,5 +1435,203 @@ describe('@stylexjs/babel-plugin', () => {
         }[!!(sidebar == null && !isSidebar) << 2 | !!isSidebar << 1 | !!isContent << 0];"
       `);
     });
+    test('Stylex call with debug on', () => {
+      expect(
+        transform(
+          `
+          import * as stylex from '@stylexjs/stylex';
+          const styles = stylex.create({
+            sidebar: {
+              boxSizing: 'border-box',
+              gridArea: 'sidebar',
+            },
+            content: {
+              gridArea: 'content',
+            },
+            root: {
+              display: 'grid',
+              gridTemplateRows: '100%',
+              gridTemplateAreas: '"content"',
+            },
+            withSidebar: {
+              gridTemplateColumns: 'auto minmax(0, 1fr)',
+              gridTemplateRows: '100%',
+              gridTemplateAreas: '"sidebar content"',
+              '@media (max-width: 640px)': {
+                gridTemplateRows: 'minmax(0, 1fr) auto',
+                gridTemplateAreas: '"content" "sidebar"',
+                gridTemplateColumns: '100%',
+              },
+            },
+            noSidebar: {
+              gridTemplateColumns: 'minmax(0, 1fr)',
+            },
+          });
+          const complex = stylex.props([
+            styles.root,
+            sidebar == null && !isSidebar ? styles.noSidebar : styles.withSidebar,
+            isSidebar && styles.sidebar,
+            isContent && styles.content,
+          ]);
+        `,
+          {
+            filename: '/html/js/FooBar.react.js',
+            dev: true,
+            debug: true,
+            enableDebugClassNames: true,
+            genConditionalClasses: true,
+          },
+        ),
+      ).toMatchInlineSnapshot(`
+        "import _inject from "@stylexjs/stylex/lib/stylex-inject";
+        var _inject2 = _inject;
+        import * as stylex from '@stylexjs/stylex';
+        _inject2(".boxSizing-x9f619{box-sizing:border-box}", 3000);
+        _inject2(".gridArea-x1yc5d2u{grid-area:sidebar}", 1000);
+        _inject2(".gridArea-x1fdo2jl{grid-area:content}", 1000);
+        _inject2(".display-xrvj5dj{display:grid}", 3000);
+        _inject2(".gridTemplateRows-x7k18q3{grid-template-rows:100%}", 3000);
+        _inject2(".gridTemplateAreas-x5gp9wm{grid-template-areas:\\"content\\"}", 2000);
+        _inject2(".gridTemplateColumns-x1rkzygb{grid-template-columns:auto minmax(0,1fr)}", 3000);
+        _inject2(".gridTemplateAreas-x17lh93j{grid-template-areas:\\"sidebar content\\"}", 2000);
+        _inject2("@media (max-width: 640px){.gridTemplateRows-xmr4b4k.gridTemplateRows-xmr4b4k{grid-template-rows:minmax(0,1fr) auto}}", 3200);
+        _inject2("@media (max-width: 640px){.gridTemplateAreas-xesbpuc.gridTemplateAreas-xesbpuc{grid-template-areas:\\"content\\" \\"sidebar\\"}}", 2200);
+        _inject2("@media (max-width: 640px){.gridTemplateColumns-x15nfgh4.gridTemplateColumns-x15nfgh4{grid-template-columns:100%}}", 3200);
+        _inject2(".gridTemplateColumns-x1mkdm3x{grid-template-columns:minmax(0,1fr)}", 3000);
+        const complex = {
+          0: {
+            className: "display-xrvj5dj gridTemplateColumns-x1rkzygb gridTemplateRows-x7k18q3 gridTemplateAreas-x17lh93j gridTemplateRows-xmr4b4k gridTemplateAreas-xesbpuc gridTemplateColumns-x15nfgh4",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:16"
+          },
+          4: {
+            className: "display-xrvj5dj gridTemplateRows-x7k18q3 gridTemplateAreas-x5gp9wm gridTemplateColumns-x1mkdm3x",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:26"
+          },
+          2: {
+            className: "display-xrvj5dj gridTemplateColumns-x1rkzygb gridTemplateRows-x7k18q3 gridTemplateAreas-x17lh93j gridTemplateRows-xmr4b4k gridTemplateAreas-xesbpuc gridTemplateColumns-x15nfgh4 boxSizing-x9f619 gridArea-x1yc5d2u",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:16; js/FooBar.react.js:4"
+          },
+          6: {
+            className: "display-xrvj5dj gridTemplateRows-x7k18q3 gridTemplateAreas-x5gp9wm gridTemplateColumns-x1mkdm3x boxSizing-x9f619 gridArea-x1yc5d2u",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:26; js/FooBar.react.js:4"
+          },
+          1: {
+            className: "display-xrvj5dj gridTemplateColumns-x1rkzygb gridTemplateRows-x7k18q3 gridTemplateAreas-x17lh93j gridTemplateRows-xmr4b4k gridTemplateAreas-xesbpuc gridTemplateColumns-x15nfgh4 gridArea-x1fdo2jl",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:16; js/FooBar.react.js:8"
+          },
+          5: {
+            className: "display-xrvj5dj gridTemplateRows-x7k18q3 gridTemplateAreas-x5gp9wm gridTemplateColumns-x1mkdm3x gridArea-x1fdo2jl",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:26; js/FooBar.react.js:8"
+          },
+          3: {
+            className: "display-xrvj5dj gridTemplateColumns-x1rkzygb gridTemplateRows-x7k18q3 gridTemplateAreas-x17lh93j gridTemplateRows-xmr4b4k gridTemplateAreas-xesbpuc gridTemplateColumns-x15nfgh4 boxSizing-x9f619 gridArea-x1fdo2jl",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:16; js/FooBar.react.js:4; js/FooBar.react.js:8"
+          },
+          7: {
+            className: "display-xrvj5dj gridTemplateRows-x7k18q3 gridTemplateAreas-x5gp9wm gridTemplateColumns-x1mkdm3x boxSizing-x9f619 gridArea-x1fdo2jl",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:26; js/FooBar.react.js:4; js/FooBar.react.js:8"
+          }
+        }[!!(sidebar == null && !isSidebar) << 2 | !!isSidebar << 1 | !!isContent << 0];"
+      `);
+    });
+    test('Stylex call with debug on and debug classnames off', () => {
+      expect(
+        transform(
+          `
+          import * as stylex from '@stylexjs/stylex';
+          const styles = stylex.create({
+            sidebar: {
+              boxSizing: 'border-box',
+              gridArea: 'sidebar',
+            },
+            content: {
+              gridArea: 'content',
+            },
+            root: {
+              display: 'grid',
+              gridTemplateRows: '100%',
+              gridTemplateAreas: '"content"',
+            },
+            withSidebar: {
+              gridTemplateColumns: 'auto minmax(0, 1fr)',
+              gridTemplateRows: '100%',
+              gridTemplateAreas: '"sidebar content"',
+              '@media (max-width: 640px)': {
+                gridTemplateRows: 'minmax(0, 1fr) auto',
+                gridTemplateAreas: '"content" "sidebar"',
+                gridTemplateColumns: '100%',
+              },
+            },
+            noSidebar: {
+              gridTemplateColumns: 'minmax(0, 1fr)',
+            },
+          });
+          const complex = stylex.props([
+            styles.root,
+            sidebar == null && !isSidebar ? styles.noSidebar : styles.withSidebar,
+            isSidebar && styles.sidebar,
+            isContent && styles.content,
+          ]);
+        `,
+          {
+            filename: '/html/js/FooBar.react.js',
+            dev: true,
+            debug: true,
+            enableDebugClassNames: false,
+            genConditionalClasses: true,
+          },
+        ),
+      ).toMatchInlineSnapshot(`
+        "import _inject from "@stylexjs/stylex/lib/stylex-inject";
+        var _inject2 = _inject;
+        import * as stylex from '@stylexjs/stylex';
+        _inject2(".x9f619{box-sizing:border-box}", 3000);
+        _inject2(".x1yc5d2u{grid-area:sidebar}", 1000);
+        _inject2(".x1fdo2jl{grid-area:content}", 1000);
+        _inject2(".xrvj5dj{display:grid}", 3000);
+        _inject2(".x7k18q3{grid-template-rows:100%}", 3000);
+        _inject2(".x5gp9wm{grid-template-areas:\\"content\\"}", 2000);
+        _inject2(".x1rkzygb{grid-template-columns:auto minmax(0,1fr)}", 3000);
+        _inject2(".x17lh93j{grid-template-areas:\\"sidebar content\\"}", 2000);
+        _inject2("@media (max-width: 640px){.xmr4b4k.xmr4b4k{grid-template-rows:minmax(0,1fr) auto}}", 3200);
+        _inject2("@media (max-width: 640px){.xesbpuc.xesbpuc{grid-template-areas:\\"content\\" \\"sidebar\\"}}", 2200);
+        _inject2("@media (max-width: 640px){.x15nfgh4.x15nfgh4{grid-template-columns:100%}}", 3200);
+        _inject2(".x1mkdm3x{grid-template-columns:minmax(0,1fr)}", 3000);
+        const complex = {
+          0: {
+            className: "xrvj5dj x1rkzygb x7k18q3 x17lh93j xmr4b4k xesbpuc x15nfgh4",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:16"
+          },
+          4: {
+            className: "xrvj5dj x7k18q3 x5gp9wm x1mkdm3x",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:26"
+          },
+          2: {
+            className: "xrvj5dj x1rkzygb x7k18q3 x17lh93j xmr4b4k xesbpuc x15nfgh4 x9f619 x1yc5d2u",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:16; js/FooBar.react.js:4"
+          },
+          6: {
+            className: "xrvj5dj x7k18q3 x5gp9wm x1mkdm3x x9f619 x1yc5d2u",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:26; js/FooBar.react.js:4"
+          },
+          1: {
+            className: "xrvj5dj x1rkzygb x7k18q3 x17lh93j xmr4b4k xesbpuc x15nfgh4 x1fdo2jl",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:16; js/FooBar.react.js:8"
+          },
+          5: {
+            className: "xrvj5dj x7k18q3 x5gp9wm x1mkdm3x x1fdo2jl",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:26; js/FooBar.react.js:8"
+          },
+          3: {
+            className: "xrvj5dj x1rkzygb x7k18q3 x17lh93j xmr4b4k xesbpuc x15nfgh4 x9f619 x1fdo2jl",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:16; js/FooBar.react.js:4; js/FooBar.react.js:8"
+          },
+          7: {
+            className: "xrvj5dj x7k18q3 x5gp9wm x1mkdm3x x9f619 x1fdo2jl",
+            "data-style-src": "js/FooBar.react.js:11; js/FooBar.react.js:26; js/FooBar.react.js:4; js/FooBar.react.js:8"
+          }
+        }[!!(sidebar == null && !isSidebar) << 2 | !!isSidebar << 1 | !!isContent << 0];"
+      `);
+    });
   });
 });
