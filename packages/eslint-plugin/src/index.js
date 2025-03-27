1a29a66781be07032df9613eb4105ccb41613de1
vincentriemer@vincentriemer-mac <>
2025-03-27 11:47:07 -0700

    feat: add new eslint rule for flagging legacy media query/pseudo class syntax
    # Summary:
    
    This diff adds a new eslint rule for flagging usage of stylex's legacy media query/pseudo class syntax that looks like this:
    
    ```js
    const styles = stylex.create({
      root: {
        width: '100%',
       '@media (min-width: 600px)': {
          width: '50%',
        }
      }
    });
    ```
    
    The lint messages direct developers to instead use this syntax:
    
    ```js
    const styles = stylex.create({
      root: {
        width: {
          default: '100%',
          '@media (min-width: 600px)': '50%'
        }
      }
    });
    ```
    
    The new lint rule is notably not auto-fixable in this diff but that is something I'm going to be working on next.
    
    # Test Plan:
    
    I introduced new eslint "RuleTester" tests that are passing.

--- packages/eslint-plugin/src/index.js
+++ packages/eslint-plugin/src/index.js
@@ -11,17 +11,20 @@ import validStyles from './stylex-valid-styles';
 import sortKeys from './stylex-sort-keys';
 import validShorthands from './stylex-valid-shorthands';
 import noUnused from './stylex-no-unused';
+import noLegacyMediaQueries from './stylex-no-legacy-media-queries';
 
 const rules: {
   'valid-styles': typeof validStyles,
   'sort-keys': typeof sortKeys,
   'valid-shorthands': typeof validShorthands,
   'no-unused': typeof noUnused,
+  'no-legacy-media-queries': typeof noLegacyMediaQueries,
 } = {
   'valid-styles': validStyles,
   'sort-keys': sortKeys,
   'valid-shorthands': validShorthands,
   'no-unused': noUnused,
+  'no-legacy-media-queries': noLegacyMediaQueries,
 };
 
 export { rules };
