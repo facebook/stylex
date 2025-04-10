f1df6d68c0fcf504f66af65e9108d8e8a2e53727
Nicolas Gallagher <necolas@meta.com>
2025-04-10 16:48:09 -0700

    [babel-plugin] Refactor transform-import-test
    * Rename test to transform-import-export-test
    * Add fixture factories to help:
      a) Assert identical metadata across tests
      b) Test all exports from StyleX

---
+++ packages/babel-plugin/__tests__/transform-import-export-test.js
@@ -0,0 +1,538 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+jest.autoMockOff();
+
+const { transformSync } = require('@babel/core');
+const stylexPlugin = require('../src/index');
+
+function transform(source, opts = {}) {
+  const { code, metadata } = transformSync(source, {
+    filename: opts.filename || '/stylex/packages/fixture.stylex.js',
+    parserOpts: {
+      flow: 'all',
+    },
+    plugins: [
+      [
+        stylexPlugin,
+        {
+          unstable_moduleResolution: {
+            rootDir: '/stylex/packages/',
+            type: 'commonJS',
+          },
+          ...opts,
+        },
+      ],
+    ],
+  });
+
+  return { code, metadata };
+}
+
+/**
+ * Fixture factory
+ *
+ * This is used to create a consistent fixture across all the tests for
+ * different ways of importing StyleX. The fixture uses all of the
+ * StyleX exports to make sure they are transformed as expected.
+ */
+
+const defaultImportSource = {
+  imprt: '* as stylex',
+  as: 'stylex',
+  from: '@stylexjs/stylex',
+};
+const defaultImportMap = {
+  create: 'create',
+  createTheme: 'createTheme',
+  defineVars: 'defineVars',
+  firstThatWorks: 'firstThatWorks',
+  keyframes: 'keyframes',
+  props: 'props',
+};
+
+function createStylesFixture(
+  // Used to configure the import syntax and sources
+  { imprt, as, from } = defaultImportSource,
+  // The importMap is used to check that renamed exports works
+  importMap = defaultImportMap,
+) {
+  const { create, createTheme, defineVars, firstThatWorks, keyframes, props } =
+    importMap;
+  const scope = as != null ? as + '.' : '';
+
+  // Generate the defineVars output first.
+  // This is inlined into the fixture so that createTheme works.
+  const defineVarsOutput = transform(
+    `
+    import ${imprt} from "${from}";
+    export const vars = ${scope}${defineVars}({
+      bar: 'left'
+    });
+  `,
+    {
+      filename: '/stylex/packages/vars.stylex.js',
+    },
+  ).code;
+
+  return `
+    ${defineVarsOutput}
+    const styles = ${scope}${create}({
+      root: {
+        animationName: ${scope}${keyframes}({
+          from: {
+            backgroundColor: 'yellow'
+          },
+          to: {
+            backgroundColor: 'orange'
+          },
+        }),
+        color: 'red',
+        position: ${scope}${firstThatWorks}('sticky', 'fixed')
+      }
+    });
+
+    const theme = ${scope}${createTheme}(vars, {
+      bar: 'green'
+    });
+
+    ${scope}${props}(styles.root, theme);
+  `;
+}
+
+/**
+ * Tests
+ */
+
+describe('@stylexjs/babel-plugin', () => {
+  describe('[transform] stylex imports', () => {
+    let expectedImportTestMetadata = null;
+    beforeEach(() => {
+      // import tests (using the fixture) should produce the same metadata
+      expectedImportTestMetadata = transform(createStylesFixture()).metadata;
+    });
+
+    test('import: none', () => {
+      const { code, metadata } = transform(`
+        export const styles = stylex.create({
+          root: {
+            color: 'red'
+          }
+        });
+      `);
+
+      expect(code).toMatchInlineSnapshot(`
+        "export const styles = stylex.create({
+          root: {
+            color: 'red'
+          }
+        });"
+      `);
+
+      expect(metadata).toMatchInlineSnapshot(`
+        {
+          "stylex": [],
+        }
+      `);
+    });
+
+    test('import: non-stylex', () => {
+      const { code, metadata } = transform(`
+        import {foo, bar} from 'other';
+      `);
+
+      expect(code).toMatchInlineSnapshot(
+        '"import { foo, bar } from \'other\';"',
+      );
+
+      expect(metadata).toMatchInlineSnapshot(`
+        {
+          "stylex": [],
+        }
+      `);
+    });
+
+    test('require: non-stylex', () => {
+      const { code, metadata } = transform(`
+        const {foo, bar} = require('other');
+      `);
+
+      expect(code).toMatchInlineSnapshot(`
+        "const {
+          foo,
+          bar
+        } = require('other');"
+      `);
+
+      expect(metadata).toMatchInlineSnapshot(`
+        {
+          "stylex": [],
+        }
+      `);
+    });
+
+    test('import: wildcard', () => {
+      const fixture = createStylesFixture({
+        imprt: '* as stylex',
+        as: 'stylex',
+        from: '@stylexjs/stylex',
+      });
+
+      const { code, metadata } = transform(fixture);
+
+      expect(code).toMatchInlineSnapshot(`
+        "import * as stylex from "@stylexjs/stylex";
+        export const vars = {
+          bar: "var(--x1hi1hmf)",
+          __themeName__: "xop34xu"
+        };
+        const styles = {
+          root: {
+            kKVMdj: "x1qar0u3",
+            kMwMTN: "x1e2nbdu",
+            kVAEAm: "x15oojuh",
+            $$css: true
+          }
+        };
+        const theme = {
+          $$css: true,
+          xop34xu: "xfnndu4 xop34xu"
+        };
+        stylex.props(styles.root, theme);"
+      `);
+
+      expect(metadata).toEqual(expectedImportTestMetadata);
+    });
+
+    test('import: wildcard (other)', () => {
+      const fixture = createStylesFixture({
+        imprt: '* as foo',
+        as: 'foo',
+        from: '@stylexjs/stylex',
+      });
+
+      const { code, metadata } = transform(fixture);
+
+      expect(code).toMatchInlineSnapshot(`
+        "import * as foo from "@stylexjs/stylex";
+        export const vars = {
+          bar: "var(--x1hi1hmf)",
+          __themeName__: "xop34xu"
+        };
+        const styles = {
+          root: {
+            kKVMdj: "x1qar0u3",
+            kMwMTN: "x1e2nbdu",
+            kVAEAm: "x15oojuh",
+            $$css: true
+          }
+        };
+        const theme = {
+          $$css: true,
+          xop34xu: "xfnndu4 xop34xu"
+        };
+        foo.props(styles.root, theme);"
+      `);
+
+      expect(metadata).toEqual(expectedImportTestMetadata);
+    });
+
+    test('import: named', () => {
+      const fixture = createStylesFixture({
+        imprt:
+          '{create, createTheme, defineVars, firstThatWorks, keyframes, props}',
+        from: '@stylexjs/stylex',
+      });
+
+      const { code, metadata } = transform(fixture);
+
+      expect(code).toMatchInlineSnapshot(`
+        "import { create, createTheme, defineVars, firstThatWorks, keyframes, props } from "@stylexjs/stylex";
+        export const vars = {
+          bar: "var(--x1hi1hmf)",
+          __themeName__: "xop34xu"
+        };
+        const styles = {
+          root: {
+            kKVMdj: "x1qar0u3",
+            kMwMTN: "x1e2nbdu",
+            kVAEAm: "x15oojuh",
+            $$css: true
+          }
+        };
+        const theme = {
+          $$css: true,
+          xop34xu: "xfnndu4 xop34xu"
+        };
+        props(styles.root, theme);"
+      `);
+
+      expect(metadata).toEqual(expectedImportTestMetadata);
+    });
+
+    test('import: named alias', () => {
+      const fixture = createStylesFixture(
+        {
+          imprt: `{
+          create as _create,
+          createTheme as _createTheme,
+          defineVars as _defineVars,
+          firstThatWorks as _firstThatWorks,
+          keyframes as _keyframes,
+          props as _props
+        }`,
+          from: '@stylexjs/stylex',
+        },
+        {
+          create: '_create',
+          createTheme: '_createTheme',
+          defineVars: '_defineVars',
+          firstThatWorks: '_firstThatWorks',
+          keyframes: '_keyframes',
+          props: '_props',
+        },
+      );
+
+      const { code, metadata } = transform(fixture);
+
+      expect(code).toMatchInlineSnapshot(`
+        "import { create as _create, createTheme as _createTheme, defineVars as _defineVars, firstThatWorks as _firstThatWorks, keyframes as _keyframes, props as _props } from "@stylexjs/stylex";
+        export const vars = {
+          bar: "var(--x1hi1hmf)",
+          __themeName__: "xop34xu"
+        };
+        const styles = {
+          root: {
+            kKVMdj: "x1qar0u3",
+            kMwMTN: "x1e2nbdu",
+            kVAEAm: "x15oojuh",
+            $$css: true
+          }
+        };
+        const theme = {
+          $$css: true,
+          xop34xu: "xfnndu4 xop34xu"
+        };
+        _props(styles.root, theme);"
+      `);
+
+      expect(metadata).toEqual(expectedImportTestMetadata);
+    });
+
+    test('importSources (string)', () => {
+      const fixture = createStylesFixture({
+        imprt: '* as stylex',
+        as: 'stylex',
+        from: 'foo-bar',
+      });
+
+      const options = { importSources: ['foo-bar'] };
+      const { code, metadata } = transform(fixture, options);
+
+      expect(code).toMatchInlineSnapshot(`
+        "import * as stylex from "foo-bar";
+        export const vars = {
+          bar: "var(--xx0id05)",
+          __themeName__: "xvxi5qu"
+        };
+        const styles = {
+          root: {
+            kKVMdj: "x1qar0u3",
+            kMwMTN: "x1e2nbdu",
+            kVAEAm: "x15oojuh",
+            $$css: true
+          }
+        };
+        const theme = {
+          $$css: true,
+          xvxi5qu: "x1lef5kd xvxi5qu"
+        };
+        stylex.props(styles.root, theme);"
+      `);
+
+      expect(metadata).toEqual(expectedImportTestMetadata);
+    });
+
+    test('importSources (object)', () => {
+      const fixture = createStylesFixture({
+        imprt: '{css, html}',
+        as: 'css',
+        from: 'react-strict-dom',
+      });
+
+      // as: aliases the exports (e.g., "* as stylex" => "css")
+      // from: aliases the package name (e.g., "@stylexjs/stylex" => "react-strict-dom")
+      const options = {
+        importSources: [{ as: 'css', from: 'react-strict-dom' }],
+      };
+
+      const { code, metadata } = transform(fixture, options);
+
+      expect(code).toMatchInlineSnapshot(`
+        "import { css, html } from "react-strict-dom";
+        export const vars = {
+          bar: "var(--xx0id05)",
+          __themeName__: "xvxi5qu"
+        };
+        const styles = {
+          root: {
+            kKVMdj: "x1qar0u3",
+            kMwMTN: "x1e2nbdu",
+            kVAEAm: "x15oojuh",
+            $$css: true
+          }
+        };
+        const theme = {
+          $$css: true,
+          xvxi5qu: "x1lef5kd xvxi5qu"
+        };
+        css.props(styles.root, theme);"
+      `);
+
+      expect(metadata).toEqual(expectedImportTestMetadata);
+    });
+
+    // This is only used at Meta, and not supported in OSS by the runtime.
+    // This tests both the built-in "importSource" of "stylex", and special handling of default export.
+    test('[META-ONLY] import: default', () => {
+      const fixture = createStylesFixture({
+        imprt: 'stylex',
+        as: 'stylex',
+        from: 'stylex',
+      });
+
+      const { code, metadata } = transform(fixture);
+
+      expect(code).toMatchInlineSnapshot(`
+        "import stylex from "stylex";
+        export const vars = {
+          bar: "var(--x1hi1hmf)",
+          __themeName__: "xop34xu"
+        };
+        const styles = {
+          root: {
+            kKVMdj: "x1qar0u3",
+            kMwMTN: "x1e2nbdu",
+            kVAEAm: "x15oojuh",
+            $$css: true
+          }
+        };
+        const theme = {
+          $$css: true,
+          xop34xu: "xfnndu4 xop34xu"
+        };
+        stylex.props(styles.root, theme);"
+      `);
+
+      expect(metadata).toEqual(expectedImportTestMetadata);
+    });
+  });
+
+  describe('[transform] stylex exports', () => {
+    let expectedExportTestMetadata = null;
+    const fixture = `stylex.create({
+      root: {
+        color: 'red',
+      }
+    })`;
+
+    beforeEach(() => {
+      // Export tests (using the fixture) should produce the same metadata
+      expectedExportTestMetadata = transform(
+        `
+        import * as stylex from '@stylexjs/stylex';
+        const styles = ${fixture};
+      `,
+        {
+          filename: '/stylex/packages/vars.stylex.js',
+        },
+      ).metadata;
+    });
+
+    test('export: named property', () => {
+      const { code, metadata } = transform(`
+        import * as stylex from '@stylexjs/stylex';
+        const styles = ${fixture};
+        export {styles}
+      `);
+
+      expect(code).toMatchInlineSnapshot(`
+        "import * as stylex from '@stylexjs/stylex';
+        const styles = {
+          root: {
+            kMwMTN: "x1e2nbdu",
+            $$css: true
+          }
+        };
+        export { styles };"
+      `);
+
+      expect(metadata).toEqual(expectedExportTestMetadata);
+    });
+
+    test('export: named declaration', () => {
+      const { code, metadata } = transform(`
+        import * as stylex from '@stylexjs/stylex';
+        export const styles = ${fixture};
+      `);
+
+      expect(code).toMatchInlineSnapshot(`
+        "import * as stylex from '@stylexjs/stylex';
+        export const styles = {
+          root: {
+            kMwMTN: "x1e2nbdu",
+            $$css: true
+          }
+        };"
+      `);
+
+      expect(metadata).toEqual(expectedExportTestMetadata);
+    });
+
+    test('export: default', () => {
+      const { code, metadata } = transform(`
+        import * as stylex from '@stylexjs/stylex';
+        export default (${fixture});
+      `);
+
+      expect(code).toMatchInlineSnapshot(`
+        "import * as stylex from '@stylexjs/stylex';
+        export default {
+          root: {
+            kMwMTN: "x1e2nbdu",
+            $$css: true
+          }
+        };"
+      `);
+
+      expect(metadata).toEqual(expectedExportTestMetadata);
+    });
+
+    test('module.export', () => {
+      const { code, metadata } = transform(`
+        import * as stylex from '@stylexjs/stylex';
+        const styles = ${fixture};
+        module.export = styles;
+      `);
+
+      expect(code).toMatchInlineSnapshot(`
+        "import * as stylex from '@stylexjs/stylex';
+        const styles = {
+          root: {
+            kMwMTN: "x1e2nbdu",
+            $$css: true
+          }
+        };
+        module.export = styles;"
+      `);
+
+      expect(metadata).toEqual(expectedExportTestMetadata);
+    });
+  });
+});
