1c28907015d351a0189f317a25faa4e5045fa8c3
Melissa Liu <mellyeliu@meta.com>
2025-04-25 00:51:08 -0700

    [style-value-parser] chore: remove character based parser

--- packages/style-value-parser/src/properties.js
+++ packages/style-value-parser/src/properties.js
@@ -7,23 +7,6 @@
  * @flow strict
  */
 
-import { Parser, type FromParser } from './core';
-import { cssWideKeywords } from './css-types/common-types';
-
-export const appearance: Parser<
-  | FromParser<typeof cssWideKeywords>
-  | 'none'
-  | 'auto'
-  | 'menulist-button'
-  | 'textfield',
-> = Parser.oneOf(
-  cssWideKeywords,
-  Parser.string('none'),
-  Parser.string('auto'),
-  Parser.string('menulist-button'),
-  Parser.string('textfield'),
-);
-
 export { Transform } from './properties/transform';
 export { BoxShadow, BoxShadowList } from './properties/box-shadow';
 export {
