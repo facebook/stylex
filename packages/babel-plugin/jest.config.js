8a9105033b292198ff65790ce2bc921d08700547
Nicolas Gallagher <necolas@meta.com>
2025-05-01 11:56:34 -0700

    Add jest coverage thresholds to packages
    Help prevent regressions to code coverage for babel-plugin,
    eslint-plugin, style-value-parser, and stylex packages. The thresholds
    for packages with low coverage should be increased as coverage improves.

---
+++ packages/babel-plugin/jest.config.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+module.exports = {
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{js,jsx}',
+    // exclude
+    '!<rootDir>/src/**/__tests__/**',
+    '!<rootDir>/src/**/tests/**',
+    '!<rootDir>/src/**/types/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 74,
+      functions: 59,
+      lines: 78,
+      statements: 78,
+    },
+  },
+  snapshotFormat: {
+    printBasicPrototype: false,
+  },
+  verbose: true,
+};
