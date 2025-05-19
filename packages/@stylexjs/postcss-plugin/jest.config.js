20592c797ff2d1ac9f191ab505336d4b74707d91
Tsubasa Sakai <javascripter@tsukkun.net>
2025-05-19 17:14:12 +0900

    [postcss-plugin] move jest config from package.json to jest.config.js in tests

---
+++ packages/@stylexjs/postcss-plugin/jest.config.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+module.exports = {
+  testPathIgnorePatterns: ['/__fixtures__/'],
+  testEnvironment: 'node',
+};
