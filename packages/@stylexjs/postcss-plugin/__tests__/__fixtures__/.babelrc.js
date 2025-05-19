ce048a61aca0764a5f77b827e8517005d307e2a2
Tsubasa Sakai <javascripter@tsukkun.net>
2025-05-19 17:20:11 +0900

    [postcss-plugin] remove devDependencies already installed at root  in postcss-plugin and remove @babel/preset-env from both babel config and devDependencies

--- packages/@stylexjs/postcss-plugin/__tests__/__fixtures__/.babelrc.js
+++ packages/@stylexjs/postcss-plugin/__tests__/__fixtures__/.babelrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
   plugins: [
     ['@stylexjs/babel-plugin', { dev: false, runtimeInjection: false }],
   ],
