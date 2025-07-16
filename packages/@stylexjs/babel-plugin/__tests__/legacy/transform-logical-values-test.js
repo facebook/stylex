c79f9a78385992beedd26e3d0ebf05de78567b66
Nicolas Gallagher <necolas@meta.com>
2025-07-16 13:54:33 -0700

    [babel-plugin][legacy] Respect enableLegacyValueFlipping in keyframes
    Fixes a bug from e9cde381365367a7a7f3eb9a547debe61a428651
    
    Make sure keyframes still flip legacy values if the option is enabled.

--- packages/@stylexjs/babel-plugin/__tests__/legacy/transform-logical-values-test.js
+++ packages/@stylexjs/babel-plugin/__tests__/legacy/transform-logical-values-test.js
@@ -301,6 +301,35 @@ describe('@stylexjs/babel-plugin', () => {
         transform(
           `
           import stylex from 'stylex';
+          const styles = stylex.create({
+            x: {
+              animationName: stylex.keyframes({
+                '0%': {
+                  boxShadow: '1px 2px 3px 4px red'
+                },
+                '100%': {
+                  boxShadow: '10px 20px 30px 40px green'
+                }
+              })
+            }
+          });
+          const classnames = stylex(styles.x);
+        `,
+          { enableLegacyValueFlipping: true },
+        ),
+      ).toMatchInlineSnapshot(`
+        "import _inject from "@stylexjs/stylex/lib/stylex-inject";
+        var _inject2 = _inject;
+        import stylex from 'stylex';
+        _inject2("@keyframes x19mpx8i-B{0%{box-shadow:1px 2px 3px 4px red;}100%{box-shadow:10px 20px 30px 40px green;}}", 0, "@keyframes x19mpx8i-B{0%{box-shadow:-1px 2px 3px 4px red;}100%{box-shadow:-10px 20px 30px 40px green;}}");
+        _inject2(".x14pamct{animation-name:x19mpx8i-B}", 3000);
+        const classnames = "x14pamct";"
+      `);
+
+      expect(
+        transform(
+          `
+          import stylex from 'stylex';
           const styles = stylex.create({ x: { boxShadow: 'none' } });
           const classnames = stylex(styles.x);
         `,
