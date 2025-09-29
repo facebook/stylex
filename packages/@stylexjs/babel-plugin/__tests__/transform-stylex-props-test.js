a09a9174de7159833a34802f3554a599172f31bd
Naman Goel <nmn@users.noreply.github.com>
2025-09-29 12:29:22 -0400

    [babel-plugin] Fix hoisiting stylex.create with duplicate names correctly (#1253)

--- packages/@stylexjs/babel-plugin/__tests__/transform-stylex-props-test.js
+++ packages/@stylexjs/babel-plugin/__tests__/transform-stylex-props-test.js
@@ -1793,5 +1793,72 @@ describe('@stylexjs/babel-plugin', () => {
         }[!!(sidebar == null && !isSidebar) << 2 | !!isSidebar << 1 | !!isContent << 0];"
       `);
     });
+
+    test('hoisting correctly with duplicte names', () => {
+      expect(
+        transform(
+          `
+            import * as stylex from "@stylexjs/stylex";
+            import * as React from "react";
+
+            function Foo() {
+              const styles = stylex.create({
+                div: { color: "red" },
+              });
+              return <div {...stylex.props(styles.div)}>Hello, foo!</div>;
+            }
+
+            function Bar() {
+              const styles = stylex.create({
+                div: { color: "blue" },
+              });
+              return <div {...stylex.props(styles.div)}>Hello, bar!</div>;
+            }
+
+            export function App() {
+              return (
+                <>
+                  <Foo />
+                  <Bar />
+                </>
+              );
+            }
+          `,
+        ),
+      ).toMatchInlineSnapshot(`
+        "import _inject from "@stylexjs/stylex/lib/stylex-inject";
+        var _inject2 = _inject;
+        import * as stylex from "@stylexjs/stylex";
+        import * as React from "react";
+        _inject2(".x1e2nbdu{color:red}", 3000);
+        const _styles = {
+          div: {
+            kMwMTN: "x1e2nbdu",
+            $$css: true
+          }
+        };
+        function Foo() {
+          const styles = _styles;
+          return <div {...stylex.props(styles.div)}>Hello, foo!</div>;
+        }
+        _inject2(".xju2f9n{color:blue}", 3000);
+        const _styles2 = {
+          div: {
+            kMwMTN: "xju2f9n",
+            $$css: true
+          }
+        };
+        function Bar() {
+          const styles = _styles2;
+          return <div {...stylex.props(styles.div)}>Hello, bar!</div>;
+        }
+        export function App() {
+          return <>
+                          <Foo />
+                          <Bar />
+                        </>;
+        }"
+      `);
+    });
   });
 });
