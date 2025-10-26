489d271c54fc0c5426c54857647a27e2981fe964
Naman Goel <naman34@gmail.com>
2025-10-26 15:29:22 -0700

    A imple Vite example

---
+++ examples/example-vite/src/App.jsx
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import * as stylex from '@stylexjs/stylex';
+
+const styles = stylex.create({
+  app: {
+    minHeight: '100%',
+    display: 'grid',
+    placeItems: 'center',
+  },
+  title: {
+    color: '#0b5fff',
+    fontSize: 28,
+    fontWeight: 700,
+  },
+});
+
+export default function App() {
+  return (
+    <main className={stylex(styles.app)}>
+      <h1 className={stylex(styles.title)}>StyleX + Vite + unplugin</h1>
+    </main>
+  );
+}
