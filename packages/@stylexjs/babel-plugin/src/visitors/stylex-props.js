a5890c2b44d6a8cf9a2b42ad8f8114fa733f6bbf
Naman Goel <nmn@users.noreply.github.com>
2025-11-11 01:55:57 -0500

    Enhance `stylex.props` to precompile more often (#1242)
    * Add stylex.defaultTarget
    
    * Rename defaultTarget to defaultMarker
    
    * fix using imported values from .stylex.js files in stylex.props
    
    * Slightly further fix
    
    * [fix] Disable using imported values for stylex.props
    
    * Add additional tests for stylex.props
    
    * Update tests
    
    * Fix bugs when compiling with local arrays

--- packages/@stylexjs/babel-plugin/src/visitors/stylex-props.js
+++ packages/@stylexjs/babel-plugin/src/visitors/stylex-props.js
@@ -24,7 +24,21 @@ type StyleObject = {
   [key: string]: string | null | boolean,
 };
 
-type ConditionalStyle = [t.Expression, ?StyleObject, ?StyleObject];
+class ConditionalStyle {
+  test: t.Expression;
+  primary: ?StyleObject;
+  fallback: ?StyleObject;
+  constructor(
+    test: t.Expression,
+    primary: ?StyleObject,
+    fallback: ?StyleObject,
+  ) {
+    this.test = test;
+    this.primary = primary;
+    this.fallback = fallback;
+  }
+}
+
 type ResolvedArg = ?StyleObject | ConditionalStyle;
 type ResolvedArgs = Array<ResolvedArg>;
 
@@ -48,8 +62,6 @@ export default function transformStylexProps(
   path: NodePath<t.CallExpression>,
   state: StateManager,
 ) {
-  const { node } = path;
-
   if (
     !isCalleeIdentifier(path, state) &&
     !isCalleeMemberExpression(path, state)
@@ -60,9 +72,11 @@ export default function transformStylexProps(
   let bailOut = false;
   let conditional = 0;
 
-  const args = node.arguments.flatMap((arg) =>
-    arg.type === 'ArrayExpression' ? arg.elements : [arg],
-  );
+  const argsPath = path
+    .get('arguments')
+    .flatMap((argPath: NodePath<>) =>
+      argPath.isArrayExpression() ? argPath.get('elements') : [argPath],
+    );
 
   let currentIndex = -1;
   let bailOutIndex: ?number = null;
@@ -85,57 +99,80 @@ export default function transformStylexProps(
   const evaluatePathFnConfig: FunctionConfig = {
     identifiers,
     memberExpressions,
+    disableImports: true,
   };
 
   const resolvedArgs: ResolvedArgs = [];
-  for (const arg of args) {
+  for (const argPath of argsPath) {
     currentIndex++;
-    switch (arg.type) {
-      case 'MemberExpression': {
-        const resolved = parseNullableStyle(arg, state);
-        if (resolved === 'other') {
-          bailOutIndex = currentIndex;
-          bailOut = true;
-        } else {
-          resolvedArgs.push(resolved);
-        }
-        break;
+
+    if (
+      argPath.isObjectExpression() ||
+      argPath.isIdentifier() ||
+      argPath.isMemberExpression()
+    ) {
+      const resolved = parseNullableStyle(argPath, state, evaluatePathFnConfig);
+      if (resolved === 'other') {
+        bailOutIndex = currentIndex;
+        bailOut = true;
+      } else {
+        resolvedArgs.push(resolved);
       }
-      case 'ConditionalExpression': {
-        const { test, consequent, alternate } = arg;
-        const primary = parseNullableStyle(consequent, state);
-        const fallback = parseNullableStyle(alternate, state);
-        if (primary === 'other' || fallback === 'other') {
-          bailOutIndex = currentIndex;
-          bailOut = true;
-        } else {
-          resolvedArgs.push([test, primary, fallback]);
-          conditional++;
-        }
-        break;
+    } else if (argPath.isConditionalExpression()) {
+      const arg = argPath.node;
+      const { test } = arg;
+      const consequentPath = argPath.get('consequent');
+      const alternatePath = argPath.get('alternate');
+
+      const primary = parseNullableStyle(
+        consequentPath,
+        state,
+        evaluatePathFnConfig,
+      );
+      const fallback = parseNullableStyle(
+        alternatePath,
+        state,
+        evaluatePathFnConfig,
+      );
+      if (primary === 'other' || fallback === 'other') {
+        bailOutIndex = currentIndex;
+        bailOut = true;
+      } else {
+        resolvedArgs.push(new ConditionalStyle(test, primary, fallback));
+        conditional++;
       }
-      case 'LogicalExpression': {
-        if (arg.operator !== '&&') {
-          bailOutIndex = currentIndex;
-          bailOut = true;
-          break;
-        }
-        const { left, right } = arg;
-        const leftResolved = parseNullableStyle(left, state);
-        const rightResolved = parseNullableStyle(right, state);
-        if (leftResolved !== 'other' || rightResolved === 'other') {
-          bailOutIndex = currentIndex;
-          bailOut = true;
-        } else {
-          resolvedArgs.push([left, rightResolved, null]);
-          conditional++;
-        }
-        break;
-      }
-      default:
+    } else if (argPath.isLogicalExpression()) {
+      const arg = argPath.node;
+      if (arg.operator !== '&&') {
         bailOutIndex = currentIndex;
         bailOut = true;
         break;
+      }
+      const leftPath = argPath.get('left');
+      const rightPath = argPath.get('right');
+
+      const leftResolved = parseNullableStyle(
+        leftPath,
+        state,
+        evaluatePathFnConfig,
+      );
+      const rightResolved = parseNullableStyle(
+        rightPath,
+        state,
+        evaluatePathFnConfig,
+      );
+      if (leftResolved !== 'other' || rightResolved === 'other') {
+        bailOutIndex = currentIndex;
+        bailOut = true;
+      } else {
+        resolvedArgs.push(
+          new ConditionalStyle(leftPath.node, rightResolved, null),
+        );
+        conditional++;
+      }
+    } else {
+      bailOutIndex = currentIndex;
+      bailOut = true;
     }
     if (conditional > 4) {
       bailOut = true;
@@ -189,7 +226,11 @@ export default function transformStylexProps(
             state,
             evaluatePathFnConfig,
           );
-          if (!confident || styleValue == null) {
+          if (
+            !confident ||
+            styleValue == null ||
+            styleValue.__IS_PROXY === true
+          ) {
             nonNullProps = true;
             styleNonNullProps = true;
           } else {
@@ -278,9 +319,11 @@ export default function transformStylexProps(
 // Otherwise it returns the string "other"
 // Which is used as an indicator to bail out of this optimization.
 function parseNullableStyle(
-  node: t.Expression,
+  path: NodePath<t.Expression>,
   state: StateManager,
+  evaluatePathFnConfig: FunctionConfig,
 ): null | StyleObject | 'other' {
+  const node = path.node;
   if (
     t.isNullLiteral(node) ||
     (t.isIdentifier(node) && node.name === 'undefined')
@@ -321,13 +364,27 @@ function parseNullableStyle(
     }
   }
 
+  const parsedObj = evaluate(path, state, evaluatePathFnConfig);
+  if (
+    parsedObj.confident &&
+    parsedObj.value != null &&
+    typeof parsedObj.value === 'object'
+  ) {
+    if (parsedObj.value.__IS_PROXY === true) {
+      return 'other';
+    }
+    return parsedObj.value;
+  }
+
   return 'other';
 }
 
 function makeStringExpression(values: ResolvedArgs): t.Expression {
   const conditions = values
-    .filter((v: ResolvedArg): v is ConditionalStyle => Array.isArray(v))
-    .map((v: ConditionalStyle) => v[0]);
+    .filter(
+      (v: ResolvedArg): v is ConditionalStyle => v instanceof ConditionalStyle,
+    )
+    .map((v: ConditionalStyle) => v.test);
 
   if (conditions.length === 0) {
     const result = props(values as any);
@@ -338,8 +395,8 @@ function makeStringExpression(values: ResolvedArgs): t.Expression {
   const objEntries = conditionPermutations.map((permutation) => {
     let i = 0;
     const args = values.map((v) => {
-      if (Array.isArray(v)) {
-        const [_test, primary, fallback] = v;
+      if (v instanceof ConditionalStyle) {
+        const { primary, fallback } = v;
         return permutation[i++] ? primary : fallback;
       } else {
         return v;
