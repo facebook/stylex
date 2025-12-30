887ff55b5f05b0e9d374b12a5bc77cfee20b1810
Melissa Liu <mellyeliu@meta.com>
2025-12-29 20:34:40 -0500

    add favicon

--- packages/docs/src/pages/_layout.tsx
+++ packages/docs/src/pages/_layout.tsx
@@ -3,10 +3,14 @@ import { Provider } from '@/components/provider';
 import '@/styles/globals.css';
 import DevStyleXHMR from '@/components/DevStyleXHMR';
 import { SidebarProvider } from '@/contexts/SidebarContext';
+import faviconUrl from '@/static/img/favicon.svg';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <>
+      <head>
+        <link rel="icon" href={faviconUrl} type="image/svg+xml" />
+      </head>
       <DevStyleXHMR />
       <Provider>
         <SidebarProvider>{children}</SidebarProvider>
