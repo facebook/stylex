fcd046945795e94d8fa46aed9064339f4239a856
Alex Carpenter <im.alexcarpenter@gmail.com>
2026-01-04 18:43:13 -0500

    format

--- packages/docs/src/components/mdx/Callout.tsx
+++ packages/docs/src/components/mdx/Callout.tsx
@@ -40,10 +40,11 @@ export function Callout({ children, title, ...props }: CalloutProps) {
   );
 }
 
-export interface CalloutContainerProps extends Omit<
-  HTMLAttributes<HTMLDivElement>,
-  'className' | 'style' | 'title'
-> {
+export interface CalloutContainerProps
+  extends Omit<
+    HTMLAttributes<HTMLDivElement>,
+    'className' | 'style' | 'title'
+  > {
   /**
    * @defaultValue info
    */
@@ -93,10 +94,8 @@ export function CalloutContainer({
   );
 }
 
-export interface CalloutTitleProps extends Omit<
-  HTMLAttributes<HTMLParagraphElement>,
-  'className' | 'style'
-> {
+export interface CalloutTitleProps
+  extends Omit<HTMLAttributes<HTMLParagraphElement>, 'className' | 'style'> {
   children: ReactNode;
 }
 
@@ -108,10 +107,8 @@ export function CalloutTitle({ children, ...props }: CalloutTitleProps) {
   );
 }
 
-export interface CalloutDescriptionProps extends Omit<
-  HTMLAttributes<HTMLDivElement>,
-  'className' | 'style'
-> {
+export interface CalloutDescriptionProps
+  extends Omit<HTMLAttributes<HTMLDivElement>, 'className' | 'style'> {
   children: ReactNode;
 }
 
