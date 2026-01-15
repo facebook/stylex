c5000a34464bd114e8052bafc808612969abd4db
Sri Krishna <psk.psk6@gmail.com>
2026-01-15 10:18:29 +0530

    Add support for SvelteKit with an example

---
+++ examples/example-sveltekit/src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts" module>
+  import * as stylex from '@stylexjs/stylex';
+  import { attrs } from '$lib';
+
+  const styles = stylex.create({
+    main: {
+      width: '100vw',
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'lightblue',
+    },
+    card: {
+      backgroundColor: '#fefefe',
+      padding: '1rem',
+      borderRadius: 10,
+      justifyContent: 'center',
+      display: 'flex',
+      alignItems: 'center',
+      color: '#333',
+      fontFamily: 'Arial',
+    },
+  });
+</script>
+
+
+<div {...attrs(stylex.props(styles.main))}>
+  <div {...attrs(stylex.props(styles.card))}>Content</div>
+</div>
