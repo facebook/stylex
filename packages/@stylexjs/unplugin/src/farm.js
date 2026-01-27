f432ee225f927d8a228aa1f37719d67574ed135e
Naman Goel <nmn@users.noreply.github.com>
2026-01-27 09:00:31 -0800

    [unplugin] Bun support and refactor and improvements (#1444)
    * [WIP] Bun bundle support
    
    * [WIP] Refactor unplugin package
    
    * Fix Waku and redwood examples
    
    * Fix bun example
    
    * Rebase
    
    * Fix failing test

--- packages/@stylexjs/unplugin/src/farm.js
+++ packages/@stylexjs/unplugin/src/farm.js
@@ -4,11 +4,9 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { initClient } from 'rwsdk/client';
 
-initClient();
+import { createFarmPlugin } from 'unplugin';
 
-if (import.meta.env.DEV) {
-  // @ts-ignore
-  import('virtual:stylex:css-only');
-}
+import { unpluginFactory } from './core';
+
+export default createFarmPlugin(unpluginFactory);
