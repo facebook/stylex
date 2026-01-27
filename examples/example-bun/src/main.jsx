461cd390e5910c167bac153f5a1ff88907388a9e
Naman Goel <nmn@users.noreply.github.com>
2026-01-27 09:00:31 -0800

    [unplugin] Bun support and refactor and improvements (#1444)
    * [WIP] Bun bundle support
    
    * [WIP] Refactor unplugin package
    
    * Fix Waku and redwood examples
    
    * Fix bun example
    
    * Rebase
    
    * Fix failing test

--- examples/example-bun/src/main.jsx
+++ examples/example-bun/src/main.jsx
@@ -4,11 +4,10 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { initClient } from 'rwsdk/client';
 
-initClient();
+import './global.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 
-if (import.meta.env.DEV) {
-  // @ts-ignore
-  import('virtual:stylex:css-only');
-}
+createRoot(document.getElementById('root')).render(<App />);
