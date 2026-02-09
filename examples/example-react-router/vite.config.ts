aec401fdfd446fb7e2aa51bf7b9c4a655700cfb9
Anay Bhakat <anay@meta.com>
2026-02-09 00:49:57 -0800

    [examples] Fix compatibility issues for create-stylex-app

--- examples/example-react-router/vite.config.ts
+++ examples/example-react-router/vite.config.ts
@@ -8,11 +8,9 @@ import stylex from '@stylexjs/unplugin';
 import rsc from '@vitejs/plugin-rsc/plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
   plugins: [
-    // @ts-expect-error - no types for this yet
     stylex.vite({
       useCSSLayers: true,
       enableDebugClassNames: false,
@@ -26,7 +24,5 @@ export default defineConfig({
         ssr: 'src/entry.ssr.tsx',
       },
     }),
-    // @ts-expect-error - no types for this yet
-    devtoolsJson(),
   ],
 });
