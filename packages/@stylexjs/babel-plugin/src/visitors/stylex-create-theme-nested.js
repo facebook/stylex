8d0d5e9676943f86dbba354a45011e10ee82a1a3
Chun Wang <chunwang.me@gmail.com>
2026-04-02 15:11:57 -0400

    [4/n] Code refactoring in utils
    Summary:
    No functionality changes. Just refactor the code to be cleaner with reusable utils.

--- packages/@stylexjs/babel-plugin/src/visitors/stylex-create-theme-nested.js
+++ packages/@stylexjs/babel-plugin/src/visitors/stylex-create-theme-nested.js
@@ -8,7 +8,6 @@
  */
 
 import type { NodePath } from '@babel/traverse';
-import type { FunctionConfig } from '../utils/evaluate-path';
 import type { InjectableStyle } from '../shared';
 
 import * as t from '@babel/types';
@@ -16,215 +15,131 @@ import StateManager from '../utils/state-manager';
 import {
   createThemeNested as stylexCreateThemeNested,
   messages,
-  keyframes as stylexKeyframes,
-  positionTry as stylexPositionTry,
-  types,
 } from '../shared';
 import { convertObjectToAST } from '../utils/js-to-ast';
 import { evaluate } from '../utils/evaluate-path';
 import path from 'node:path';
+import { isCallTo, validateDefineCall, buildEvalConfig } from './visitor-utils';
 
-/// This function looks for `stylex.unstable_createThemeNested` calls and transforms them.
-/// 1. It finds the first two arguments to `stylex.unstable_createThemeNested` and validates those.
-/// 2. This handles local constants automatically.
-/// 4. It uses the `stylexCreateThemeNested` from `@stylexjs/shared` to transform the JS
-///    object and to get a list of injected styles.
-/// 5. It converts the resulting Object back into an AST and replaces the call with it.
-/// 6. It also inserts `stylex.inject` calls above the current statement as needed.
+/// Transforms `stylex.unstable_createThemeNested` calls.
+/// Validates, evaluates both arguments, delegates to the shared
+/// stylexCreateThemeNested transform, replaces the AST, and registers styles.
 export default function transformStyleXCreateThemeNested(
   callExpressionPath: NodePath<t.CallExpression>,
   state: StateManager,
 ) {
-  const callExpressionNode = callExpressionPath.node;
+  const node = callExpressionPath.node;
+  if (node.type !== 'CallExpression') return;
 
-  if (callExpressionNode.type !== 'CallExpression') {
+  if (
+    !isCallTo(
+      node,
+      state.stylexCreateThemeNestedImport,
+      'unstable_createThemeNested',
+      state.stylexImport,
+    )
+  ) {
     return;
   }
 
-  if (
-    (callExpressionNode.callee.type === 'Identifier' &&
-      state.stylexCreateThemeNestedImport.has(
-        callExpressionNode.callee.name,
-      )) ||
-    (callExpressionNode.callee.type === 'MemberExpression' &&
-      callExpressionNode.callee.object.type === 'Identifier' &&
-      callExpressionNode.callee.property.type === 'Identifier' &&
-      callExpressionNode.callee.property.name ===
-        'unstable_createThemeNested' &&
-      state.stylexImport.has(callExpressionNode.callee.object.name))
-  ) {
-    validateStyleXCreateThemeNested(callExpressionPath);
+  validateDefineCall(
+    callExpressionPath,
+    'unstable_createThemeNested',
+    2,
+    false,
+  );
 
-    // We know that parent is a variable declaration
-    const variableDeclaratorPath = callExpressionPath.parentPath;
-    if (!variableDeclaratorPath.isVariableDeclarator()) {
-      return;
-    }
-    const id = variableDeclaratorPath.get('id');
-    if (!id.isIdentifier()) {
-      return;
-    }
-    const variableName = id.node.name;
+  const variableDeclaratorPath = callExpressionPath.parentPath;
+  if (!variableDeclaratorPath.isVariableDeclarator()) return;
 
-    const args: $ReadOnlyArray<NodePath<>> =
-      callExpressionPath.get('arguments');
-    const firstArg = args[0];
-    const secondArg = args[1];
+  const id = variableDeclaratorPath.get('id');
+  if (!id.isIdentifier()) return;
+  const variableName = id.node.name;
 
-    const { confident: confident1, value: variables } = evaluate(
-      firstArg,
-      state,
-    );
-    if (!confident1) {
-      throw callExpressionPath.buildCodeFrameError(
-        messages.nonStaticValue('unstable_createThemeNested'),
-        SyntaxError,
-      );
-    }
+  const args: $ReadOnlyArray<NodePath<>> = callExpressionPath.get('arguments');
+  const firstArg = args[0];
+  const secondArg = args[1];
 
-    const otherInjectedCSSRules: { [propertyName: string]: InjectableStyle } =
-      {};
-
-    // eslint-disable-next-line no-inner-declarations
-    function keyframes<
-      Obj: {
-        +[key: string]: { +[k: string]: string | number },
-      },
-    >(animation: Obj): string {
-      const [animationName, injectedStyle] = stylexKeyframes(
-        animation,
-        state.options,
-      );
-      otherInjectedCSSRules[animationName] = injectedStyle;
-      return animationName;
-    }
-
-    // eslint-disable-next-line no-inner-declarations
-    function positionTry<Obj: { +[k: string]: string | number }>(
-      fallbackStyles: Obj,
-    ): string {
-      const [positionTryName, injectedStyle] = stylexPositionTry(
-        fallbackStyles,
-        state.options,
-      );
-      otherInjectedCSSRules[positionTryName] = injectedStyle;
-      return positionTryName;
-    }
-
-    const identifiers: FunctionConfig['identifiers'] = {};
-    const memberExpressions: FunctionConfig['memberExpressions'] = {};
-    state.stylexKeyframesImport.forEach((name) => {
-      identifiers[name] = { fn: keyframes };
-    });
-    state.stylexPositionTryImport.forEach((name) => {
-      identifiers[name] = { fn: positionTry };
-    });
-    state.stylexTypesImport.forEach((name) => {
-      identifiers[name] = types;
-    });
-    state.stylexImport.forEach((name) => {
-      if (memberExpressions[name] === undefined) {
-        memberExpressions[name] = {};
-      }
-
-      memberExpressions[name].keyframes = { fn: keyframes };
-      memberExpressions[name].positionTry = { fn: positionTry };
-      identifiers[name] = { ...(identifiers[name] ?? {}), types };
-    });
-    state.applyStylexEnv(identifiers);
-
-    const { confident: confident2, value: overrides } = evaluate(
-      secondArg,
-      state,
-      {
-        identifiers,
-        memberExpressions,
-      },
-    );
-    if (!confident2) {
-      throw callExpressionPath.buildCodeFrameError(
-        messages.nonStaticValue('unstable_createThemeNested'),
-        SyntaxError,
-      );
-    }
-    if (typeof overrides !== 'object' || overrides == null) {
-      throw callExpressionPath.buildCodeFrameError(
-        messages.nonStyleObject('unstable_createThemeNested'),
-        SyntaxError,
-      );
-    }
-
-    // Check that first arg has __varGroupHash__ set
-    if (
-      typeof variables.__varGroupHash__ !== 'string' ||
-      variables.__varGroupHash__ === ''
-    ) {
-      throw callExpressionPath.buildCodeFrameError(
-        'Can only override variables theme created with unstable_defineVarsNested().',
-        SyntaxError,
-      );
-    }
-
-    // eslint-disable-next-line prefer-const
-    let [overridesObj, injectedStyles] = stylexCreateThemeNested(
-      variables,
-      overrides,
-      state.options,
-    );
-
-    if (state.isTest) {
-      const fileName = state.filename ?? 'UnknownFile';
-      const basename = path.basename(fileName).split('.')[0];
-      const devClassName = `${basename}__${variableName}`;
-      overridesObj = {
-        [devClassName]: devClassName,
-        $$css: true,
-      };
-    } else if (state.isDev) {
-      const fileName = state.filename ?? 'UnknownFile';
-      const basename = path.basename(fileName).split('.')[0];
-      const devClassName = `${basename}__${variableName}`;
-      // $FlowFixMe[cannot-spread-indexer]
-      overridesObj = {
-        [devClassName]: devClassName,
-        ...overridesObj,
-      };
-    }
-
-    // This should be a transformed variables object
-    callExpressionPath.replaceWith(convertObjectToAST(overridesObj));
-
-    const listOfStyles = Object.entries({
-      ...otherInjectedCSSRules,
-      ...injectedStyles,
-    }).map(([key, { priority, ...rest }]) => [key, rest, priority]);
-
-    state.registerStyles(listOfStyles, variableDeclaratorPath);
-  }
-}
-
-// Validates the call of `stylex.unstable_createThemeNested`.
-function validateStyleXCreateThemeNested(
-  callExpressionPath: NodePath<t.CallExpression>,
-) {
-  const variableDeclaratorPath: any = callExpressionPath.parentPath;
-
-  if (
-    variableDeclaratorPath == null ||
-    variableDeclaratorPath.isExpressionStatement() ||
-    !variableDeclaratorPath.isVariableDeclarator() ||
-    variableDeclaratorPath.node.id.type !== 'Identifier'
-  ) {
+  // Evaluate first arg (the defineVarsNested result) without eval config
+  const { confident: confident1, value: variables } = evaluate(
+    firstArg,
+    state,
+  );
+  if (!confident1) {
     throw callExpressionPath.buildCodeFrameError(
-      messages.unboundCallValue('unstable_createThemeNested'),
+      messages.nonStaticValue('unstable_createThemeNested'),
       SyntaxError,
     );
   }
 
-  if (callExpressionPath.node.arguments.length !== 2) {
+  // Check that first arg has __varGroupHash__ set
+  if (
+    typeof variables.__varGroupHash__ !== 'string' ||
+    variables.__varGroupHash__ === ''
+  ) {
     throw callExpressionPath.buildCodeFrameError(
-      messages.illegalArgumentLength('unstable_createThemeNested', 2),
+      'Can only override variables theme created with unstable_defineVarsNested().',
       SyntaxError,
     );
   }
+
+  // Evaluate second arg (overrides) with keyframes/positionTry/types support
+  const otherInjectedCSSRules: { [string]: InjectableStyle } = {};
+  const { identifiers, memberExpressions } = buildEvalConfig(
+    state,
+    otherInjectedCSSRules,
+  );
+
+  const { confident: confident2, value: overrides } = evaluate(
+    secondArg,
+    state,
+    { identifiers, memberExpressions },
+  );
+  if (!confident2) {
+    throw callExpressionPath.buildCodeFrameError(
+      messages.nonStaticValue('unstable_createThemeNested'),
+      SyntaxError,
+    );
+  }
+  if (typeof overrides !== 'object' || overrides == null) {
+    throw callExpressionPath.buildCodeFrameError(
+      messages.nonStyleObject('unstable_createThemeNested'),
+      SyntaxError,
+    );
+  }
+
+  // eslint-disable-next-line prefer-const
+  let [overridesObj, injectedStyles] = stylexCreateThemeNested(
+    variables,
+    overrides,
+    state.options,
+  );
+
+  if (state.isTest) {
+    const fileName = state.filename ?? 'UnknownFile';
+    const basename = path.basename(fileName).split('.')[0];
+    const devClassName = `${basename}__${variableName}`;
+    overridesObj = {
+      [devClassName]: devClassName,
+      $$css: true,
+    };
+  } else if (state.isDev) {
+    const fileName = state.filename ?? 'UnknownFile';
+    const basename = path.basename(fileName).split('.')[0];
+    const devClassName = `${basename}__${variableName}`;
+    // $FlowFixMe[cannot-spread-indexer]
+    overridesObj = {
+      [devClassName]: devClassName,
+      ...overridesObj,
+    };
+  }
+
+  callExpressionPath.replaceWith(convertObjectToAST(overridesObj));
+
+  const listOfStyles = Object.entries({
+    ...otherInjectedCSSRules,
+    ...injectedStyles,
+  }).map(([key, { priority, ...rest }]) => [key, rest, priority]);
+
+  state.registerStyles(listOfStyles, variableDeclaratorPath);
 }
